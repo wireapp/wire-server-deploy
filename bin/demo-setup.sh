addf7775f7b28f755061c6a27fec2688c0ab0c09
Tiago Loureiro <tiago.loureiro@gmail.com>
2019-02-20 19:16:06 +0100

    Allow overriding resources

--- bin/demo-setup.sh
+++ bin/demo-setup.sh
@@ -3,25 +3,39 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 
 display_usage() {
-    echo "Usage: setup -o"
-    echo "  -o   use this option if you want to override default values/secrets (i.e., looks for secrets.override.yaml and values.override.yaml)"
+    echo "Usage: setup -o -b -s"
+    echo "  -o   (--override--values) default values/secrets (i.e., looks for secrets.override.yaml and values.override.yaml)"
+    echo "  -b   (--no-load-balancer) use this if you do not want to install a load balancer on the cluster (useful for cloud installations that have their own LB's)"
+    echo "  -s   (--skip-dependency-update) do not update any helm chart dependencies (can be useful for reruns)"
     exit 1
 }
 
 secretsfilename="demo-secrets.example.yaml"
 valuesfilename="demo-values.example.yaml"
+installloadbalancer=true
+updatedependencies=true
 
-while getopts ":o" opt; do
+while getopts ":obs" opt; do
   case $opt in
     o)
       secretsfilename="demo-secrets.override.yaml"
       valuesfilename="demo-values.override.yaml"
       ;;
+    b)
+      installloadbalancer=false
+      ;;
+    s)
+      updatedependencies=false
+      ;;
   esac
 done
 
-echo $secretsfilename
-echo $valuesfilename
+echo "######################################################"
+echo "Secrets: $secretsfilename"
+echo "Values: $valuesfilename"
+echo "Install loadbalancer on the cluster: $installloadbalancer"
+echo "Update dependencies: $updatedependencies"
+echo "######################################################"
 
 NAMESPACE=${NAMESPACE:-demo}
 
@@ -36,19 +50,23 @@ phase_2_charts_main=( wire-server )
 phase_3_charts_ingress=( nginx-lb-ingress )
 all_charts=( "${phase_0_charts_metallb[@]}" "${phase_1_charts_pre[@]}" "${phase_2_charts_main[@]}" "${phase_3_charts_ingress[@]}")
 
-# remove previous versions of helm charts, if any
-find "$DIR/charts" | grep ".tgz" | xargs -n 1 rm
+if [ "$updatedependencies" == true ] ; then
+    # remove previous versions of helm charts, if any
+    find "$DIR/charts" | grep ".tgz" | xargs -n 1 rm
 
-# download/refresh dependencies, if any
-helm repo add cos https://centerforopenscience.github.io/helm-charts/
-for chart in "${all_charts[@]}"; do
-    source "$DIR/bin/update.sh" "${chart}"
-done
+    # download/refresh dependencies, if any
+    helm repo add cos https://centerforopenscience.github.io/helm-charts/
+    for chart in "${all_charts[@]}"; do
+        source "$DIR/bin/update.sh" "${chart}"
+    done
+fi
 
-# Note that we should have a single metal lb in the whole cluster!
-helm upgrade --install --namespace metallb-system metallb \
-    "${DIR}/charts/metallb" -f "${DIR}/values/metallb/${valuesfilename}" \
-    --wait --timeout 1800
+if [ "$installloadbalancer" == true ] ; then
+    # Note that we should have a single metal lb in the whole cluster!
+    helm upgrade --install --namespace metallb-system metallb \
+        "${DIR}/charts/metallb" -f "${DIR}/values/metallb/${valuesfilename}" \
+        --wait --timeout 1800
+fi
 
 for chart in "${phase_1_charts_pre[@]}"; do
     valuesfile="${DIR}/values/${chart}/${valuesfilename}"
