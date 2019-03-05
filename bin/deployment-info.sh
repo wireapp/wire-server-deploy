dd4b26b30b2c8ea171d0b71b43360ed981d3d6e2
Chris Penner <christopher.penner@gmail.com>
2019-03-05 16:52:58 +0100

    Update script to add links to wire server deploy

--- bin/deployment-info.sh
+++ bin/deployment-info.sh
@@ -6,9 +6,11 @@ usage="USAGE: $0 <k8s namespace> <deployment-name (e.g. brig)>"
 namespace=${1?$usage}
 deployment=${2?$usage}
 
-repo="https://github.com/wireapp/wire-server"
+wire_server_repo="https://github.com/wireapp/wire-server"
+wire_server_deploy_repo="https://github.com/wireapp/wire-server-deploy"
 
-image=$(kubectl -n "$namespace" get deployment "$deployment" -o json |
+image=$(
+    kubectl -n "$namespace" get deployment "$deployment" -o json |
     # Filter out only pod image ids
     jq -r '.spec.template.spec.containers[].image' |
     # ignore sidecar containers, etc.
@@ -18,20 +20,27 @@ image=$(kubectl -n "$namespace" get deployment "$deployment" -o json |
 # select only docker image tag; not repo
 tag=$(echo $image | cut -f2 -d:)
 
-commit=$(
+wire_server_commit=$(
     # get all tags from repo
-    git ls-remote --tags "$repo" |
+    git ls-remote --tags "$wire_server_repo" |
     grep "image-$tag" |
     cut -f1
 )
 
-release=$(helm ls -a |
-          grep "wire-server" |
-          cut -f5
+release=$(
+    helm ls -a |
+    grep "wire-server" |
+    cut -f5
+)
+
+wire_server_deploy_commit=$(
+    git ls-remote --tags "$wire_server_deploy_repo" |
+    grep "$release" |
+    cut -f1
 )
 
 # align output nicely
 column -t <(
-    echo -e "image\trelease\tcommit\tlink"
-    echo -e "$image\t$release\t$commit\t$repo/releases/tag/image-$tag"
+    echo -e "image\trelease\twire server commit\twire server link\twire server deploy commit\twire server deploy link"
+    echo -e "$image\t$release\t$wire_server_commit\t$wire_server_repo/releases/tag/image/$tag\t$wire_server_deploy_commit\t$wire_server_deploy_repo/releases/tag/chart/$release"
 )
