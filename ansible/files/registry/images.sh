9fc629de7aa334b4b6dbaa9c0898ce2268ad8511
jschaul <jschaul@users.noreply.github.com>
2019-06-25 19:27:17 +0200

    fool DNS!

--- ansible/files/registry/images.sh
+++ ansible/files/registry/images.sh
@@ -7,25 +7,31 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 registry_name="localhost"
 
 images=$(cat $SCRIPT_DIR/list_of_docker_images.txt)
-quay=$(cat $SCRIPT_DIR/list_of_docker_images.txt | grep quay.io | awk -F quay.io/ '{print $2}' | grep -v '^$' )
-gcr=$(cat $SCRIPT_DIR/list_of_docker_images.txt | grep gcr.io | awk -F gcr.io/ '{print $2}' | grep -v '^$')
+quay=$(cat $SCRIPT_DIR/list_of_docker_images.txt | grep "^quay.io" | awk -F quay.io/ '{print $2}' | grep -v '^$' )
+gcr=$(cat $SCRIPT_DIR/list_of_docker_images.txt | grep "^gcr.io" | awk -F gcr.io/ '{print $2}' | grep -v '^$')
+k8sgcr=$(cat $SCRIPT_DIR/list_of_docker_images.txt | grep "^k8s.gcr.io" | awk -F k8s.gcr.io/ '{print $2}' | grep -v '^$')
 hub=$(cat $SCRIPT_DIR/list_of_docker_images.txt | grep -v gcr.io | grep -v quay.io)
 
 
 function mirror() {
-    docker pull $prefix/$image
-    docker tag $prefix/$image $registry_name/$image
+    docker pull $prefix$image
+    docker tag $prefix$image $registry_name/$image
     docker push $registry_name/$image
-    docker image remove $registry_name/$image
-    docker image remove $prefix/$image
+    #docker image remove $registry_name/$image
+    #docker image remove $prefix/$image
 }
 
-prefix=quay.io
+prefix=quay.io/
 for image in ${quay[@]}; do
     mirror
 done;
 
-prefix=gcr.io
+prefix=k8s.gcr.io/
+for image in ${k8sgcr[@]}; do
+    mirror
+done;
+
+prefix=gcr.io/
 for image in ${gcr[@]}; do
     mirror
 done;
