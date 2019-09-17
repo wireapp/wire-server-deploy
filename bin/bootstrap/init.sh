3987e7d021cb9eac37bd45fd6d8bdbe4bd08879f
jschaul <jschaul@users.noreply.github.com>
2019-09-17 15:52:04 +0200

    WIP2

--- bin/bootstrap/init.sh
+++ bin/bootstrap/init.sh
@@ -4,17 +4,14 @@ mkdir -p ../admin_work_dir && cd ../admin_work_dir
 mkdir -p ../dot_ssh
 mkdir -p ../dot_kube
 
-# TODO: if not exists..
-#ssh-keygen -N '' -f /root/.ssh/id_rsa
-#cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
+if [[ ! -f /root/.ssh/id_rsa ]]; then
+    # create ssh key and allow self to ssh in (from a docker image)
+    ssh-keygen -N '' -f /root/.ssh/id_rsa
+    cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
+fi
+
 # copy ssh key
 cp ~/.ssh/id_rsa ../dot_ssh/
-
-inside="cp -a /src/* /mnt
-# run ansible from here. If you make any changes, they will be written to your host file system
-# (those files will be owned by root as docker runs as root)
-cd /mnt/wire-server-deploy/ansible
-"
 
 # podman
 sudo apt-get update -qq
@@ -22,5 +19,8 @@ sudo apt-get install -qq -y software-properties-common uidmap
 sudo add-apt-repository -y ppa:projectatomic/ppa
 sudo apt-get update -qq
 sudo apt-get -qq -y install podman
+
+curl -sSfL https://raw.githubusercontent.com/wireapp/wire-server-deploy/feature/simple-bootstrap/bin/bootstrap/inside.sh > inside.sh
+chmod +x inside.sh
 
 podman run -it --network=host -v $(pwd):/mnt -v $(pwd)/../dot_ssh:/root/.ssh -v $(pwd)/../dot_kube:/root/.kube  quay.io/wire/networkless-admin
