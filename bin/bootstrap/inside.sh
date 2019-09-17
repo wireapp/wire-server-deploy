bc229d462a5060953e5801aea816810f016f98e4
jschaul <jschaul@users.noreply.github.com>
2019-09-17 16:16:09 +0200

    inside

--- bin/bootstrap/inside.sh
+++ bin/bootstrap/inside.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 
+set -ex
+
 # When this script is run the first time, copy files over
 if [[ ! -d /mnt/wire-server-deploy ]]; then
-   cp -v -a /src/* /mnt
+   cp -a /src/* /mnt
 fi
 
 # run ansible from here. If you make any changes, they will be written to your host file system
 # (those files will be owned by root as docker runs as root)
 cd /mnt/wire-server-deploy/ansible
 
-
 # This code may be brittle...
 TARGET_IFACE=$(route | grep default | awk '{print $8}')
 TARGET_HOST=$(/sbin/ifconfig $TARGET_IFACE | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 
 if [[ ! -f hosts.ini ]]; then
-   cp hosts.example.ini hosts.ini
+   cp hosts.example-demo.ini hosts.ini
    sed -i "s/X.X.X.X/$TARGET_HOST/g" hosts.ini
-   sed -i "s/# docker_dns_servers_strict = false/docker_dns_servers_strict = false/g" hosts.ini
-   sed -i "s/#docker_dns_servers_strict = false/docker_dns_servers_strict = false/g" hosts.ini
 fi
+
+
