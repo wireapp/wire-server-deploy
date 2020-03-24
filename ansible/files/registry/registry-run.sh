023eb5e8facc5543c0a58faf5e9326de11bc6e5f
jschaul <jschaul@users.noreply.github.com>
2020-03-24 18:46:50 +0100

    ninja sneak docker registry run change onto develop

--- ansible/files/registry/registry-run.sh
+++ ansible/files/registry/registry-run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+IP=${1:-"0.0.0.0"}
+PORT=${2:-"5001"}
+
 docker run \
   -d \
   --network=host \
@@ -7,7 +10,7 @@ docker run \
   --name registry \
   -v $(pwd)/../../mnt/registry:/var/lib/registry \
   -v "$(pwd)/certs:/certs" \
-  -e REGISTRY_HTTP_ADDR=0.0.0.0:5001 \
+  -e REGISTRY_HTTP_ADDR=${IP}:${PORT} \
   -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/client.crt \
   -e REGISTRY_HTTP_TLS_KEY=/certs/client.key \
   registry:2
