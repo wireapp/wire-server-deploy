db1ae9dee9601054d740459a98397e5112c5f2bf
Julia Longtin <julia.longtin@wire.com>
2020-04-24 17:04:07 +0100

    correct formatting.

--- terraform/modules/aws_vpc/variables.tf
+++ terraform/modules/aws_vpc/variables.tf
@@ -1,13 +1,16 @@
 variable "name" {
+  type        = string
   description = "VPC name as appearing in AWS"
 }
 
 variable "environment" {
-  default     = "dev"
+  type        = string
   description = "Environment name, as appears in the environment definition"
+  default     = "dev"
 }
 
 variable "dhcp_options_domain_name" {
+  type        = string
+  description = "domain name given by AWS to the instances over DHCP."
   default     = "internal.vpc"
-  description = "TODO"
 }
