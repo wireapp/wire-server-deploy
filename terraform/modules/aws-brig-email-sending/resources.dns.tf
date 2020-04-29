fc3d518e9e39cb9ee86cf33eda404f6d9b707190
Gregor <gregor.jahn@wire.com>
2020-04-29 14:27:17 +0200

    Adjust code comment

--- terraform/modules/aws-brig-email-sending/resources.dns.tf
+++ terraform/modules/aws-brig-email-sending/resources.dns.tf
@@ -2,15 +2,15 @@ resource "aws_route53_record" "domain_amazonses_verification_record" {
   zone_id = var.zone_id
   name    = "_amazonses.${var.email_domain}"
   type    = "TXT"
-  ttl     = "3600"
+  ttl     = "600"
   records = [ aws_ses_domain_identity.brig.verification_token ]
 }
 
 resource "aws_route53_record" "domain_amazonses_dkim_record" {
-  count   = var.zone_id != null ? 3 : 0
+  count   = 3
   zone_id = var.zone_id
   name    = "${element(aws_ses_domain_dkim.brig.dkim_tokens, count.index)}._domainkey.${var.email_domain}"
   type    = "CNAME"
-  ttl     = "3600"
+  ttl     = "600"
   records = [ "${element(aws_ses_domain_dkim.brig.dkim_tokens, count.index)}.dkim.amazonses.com" ]
 }
