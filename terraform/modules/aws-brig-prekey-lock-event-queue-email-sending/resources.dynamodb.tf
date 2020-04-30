b176ca6eb1f52f9e3f5a2d23c73c93ede6100fba
Gregor <gregor.jahn@wire.com>
2020-04-30 20:37:35 +0200

    Fix typo

--- terraform/modules/aws-brig-prekey-lock-event-queue-email-sending/resources.dynamodb.tf
+++ terraform/modules/aws-brig-prekey-lock-event-queue-email-sending/resources.dynamodb.tf
@@ -2,7 +2,7 @@
 # see: https://www.terraform.io/docs/providers/aws/r/appautoscaling_policy.html
 #
 resource "aws_dynamodb_table" "prekey_locks" {
-  name           = "${var.environment}-brig-prekay-locks"
+  name           = "${var.environment}-brig-prekey-locks"
   billing_mode   = "PROVISIONED"
   read_capacity  = var.prekey_table_read_capacity
   write_capacity = var.prekey_table_write_capacity
