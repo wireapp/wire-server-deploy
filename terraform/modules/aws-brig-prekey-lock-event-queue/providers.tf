a9a4a0e9eb5ab24067e10d5cc9df1cc566aeb6a4
Gregor <gregor.jahn@wire.com>
2020-04-30 20:27:59 +0200

    Integrate review feedback

--- terraform/modules/aws-brig-prekey-lock-event-queue/providers.tf
+++
@@ -1,8 +0,0 @@
-# NOTE: the provider assums that the respective environemnt variables,
-# reuqired for authentication, are being set in parent module
-#
-provider "aws" {
-  version = "~> 2.58"
-
-  region = var.region
-}
