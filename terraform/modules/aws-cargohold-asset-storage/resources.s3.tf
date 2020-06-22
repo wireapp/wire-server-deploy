23f1a21704c6b32c414051f26a9cb88890ecc3a1
jschaul <jschaul@users.noreply.github.com>
2020-06-22 19:20:45 +0200

    cargohold add CORS configuration

--- terraform/modules/aws-cargohold-asset-storage/resources.s3.tf
+++ terraform/modules/aws-cargohold-asset-storage/resources.s3.tf
@@ -2,17 +2,25 @@ resource "aws_s3_bucket" "asset_storage" {
   bucket = "${random_string.bucket.keepers.env}-${random_string.bucket.keepers.name}-cargohold-${random_string.bucket.result}"
   acl    = "private"
   region = var.region
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "HEAD"]
+    allowed_origins = ["*"]
+    max_age_seconds = 3000
+  }
+
 }
 
 resource "random_string" "bucket" {
-  length = 8
-  lower = true
-  upper = false
-  number = true
+  length  = 8
+  lower   = true
+  upper   = false
+  number  = true
   special = false
 
   keepers = {
-    env = var.environment
+    env  = var.environment
     name = var.bucket_name
   }
 }
