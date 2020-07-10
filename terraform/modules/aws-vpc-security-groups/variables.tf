140a5f03b70a0cd9bfbfc98f97c037d0b8da014b
Julia Longtin <julia.longtin@wire.com>
2020-07-10 11:29:28 +0100

    add the s3 CIDRs to the exported variables list.

--- terraform/modules/aws-vpc-security-groups/variables.tf
+++ terraform/modules/aws-vpc-security-groups/variables.tf
@@ -1,5 +1,10 @@
 variable "vpc_id" {
   type        = string
-  description = "ID of VPC these security groups are for"
+  description = "ID of VPC these security groups are for."
+}
+
+variable "s3_CIDRs" {
+  type        = list(string)
+  description = "subnets that S3 gateways we are using exist in."
 }
 
