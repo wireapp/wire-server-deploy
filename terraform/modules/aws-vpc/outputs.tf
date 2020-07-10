75192d628031ad3f2ad8d1adc28002763c003268
Julia Longtin <julia.longtin@wire.com>
2020-07-10 11:30:20 +0100

    add the CIDRs and the ID of the s3 endpoint to the list of exported objects.

--- terraform/modules/aws-vpc/outputs.tf
+++ terraform/modules/aws-vpc/outputs.tf
@@ -2,6 +2,14 @@ output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
+output "s3_endpoint_CIDRs" {
+  value = aws_vpc_endpoint.s3.cidr_blocks
+}
+
+output "s3_endpoint_id" {
+  value = aws_vpc_endpoint.s3.id
+}
+
 output "public_subnets" {
   value = module.vpc.public_subnets
 }
