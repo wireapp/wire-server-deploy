e8ecd88c822146ee98da44d0eb68b82d00ee58ba
Akshay Mankar <akshay@wire.com>
2020-09-02 17:33:32 +0200

    Create env with nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,26 @@
-{ pkgs ? import <nixpkgs> {}}:
-pkgs.mkShell {
-  buildInputs = [
-    pkgs.kubectl
-    pkgs.kubernetes-helm
-    pkgs.python27Packages.poetry
-  ];
+let
+  sources = import ./nix/sources.nix;
+
+  poetry2nixOverlay = builtins.fetchGit {
+    ref = "master";
+    rev = "ab40bbdc964b7cc0a69d6f9ce77afbc79bb6e815";
+    url = "https://github.com/nix-community/poetry2nix";
+  };
+
+  pkgs = import sources.nixpkgs {
+    overlays = [ (import "${poetry2nixOverlay}/overlay.nix") ];
+  };
+  lib = pkgs.lib;
+  poetryOverrides = pkgs.poetry2nix.mkDefaultPoetryOverrides (import ./nix/poetry-overrides.nix { inherit pkgs lib; });
+  poetryEnv = pkgs.poetry2nix.mkPoetryEnv {
+    projectDir = ./ansible;
+    python = pkgs.python37;
+    overrides = poetryOverrides;
+  };
+in
+pkgs.mkShell{
+  name = "wire-server-deploy";
+  nativeBuildInputs = [poetryEnv] ;
+  buildInputs = with pkgs; [ terraform_0_13 ];
 }
+
