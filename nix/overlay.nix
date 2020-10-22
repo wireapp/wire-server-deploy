15b24992a7a1ee670c4f3d02f47fa4a13d9cd515
Florian Klink <flokli@flokli.de>
2020-10-22 14:52:40 +0200

    remove poetry, use Nix to provide the ansible we need
    Also, set NIX_PATH when entering via direnv, so nix-shell does the right
    thing when in there.
    
    Move to ansible 2.9
    
    If we want to bump kubespray to the latest release; we need a newer
    ansible as 2.7 is not supported anymore.
    
    nix: use pkgs.ansible from nixpkgs, and python3 instead of python37
    
    Dockerfile: Include python for  localhost python interpreter
    
    Just like in hegemony
    
    pythonForAnsible: move into overlay.nix
    
    Dockerfile: stop creating the symlink from pythonForAnsible to /usr/bin/python
    
    pythonForAnsible is already part of `env`.

---
+++ nix/overlay.nix
@@ -0,0 +1,14 @@
+self: super: {
+  ansible_with_libs = super.python3Packages.toPythonApplication (super.python3Packages.ansible.overridePythonAttrs (old: rec {
+    propagatedBuildInputs = old.propagatedBuildInputs or [] ++ [
+      super.python3Packages.boto
+      super.python3Packages.boto3
+      super.python3Packages.six
+    ];
+  }));
+
+  pythonForAnsible = (self.python3.withPackages (_: self.ansible.requiredPythonModules));
+
+  kubectl_1_14_10 = self.callPackage ./pkgs/kubectl_1_14_10.nix {};
+
+}
