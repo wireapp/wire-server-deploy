46def7abc15b99b81fb2c2e25cbe531db7eddbb2
Arian van Putten <arian.vanputten@gmail.com>
2020-11-03 12:46:03 +0100

    add ci

---
+++ ci.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Build and install the environment
+nix-env -f default.nix -iA env
+
+# Build the debs
+# TODO: stable key
+mirror-bionic static/debs \
+  python-apt aufs-tools apt-transport-https software-properties-common conntrack ipvsadm ipset curl rsync socat unzip e2fsprogs xfsprogs ebtables python3-minimal \
+  openjdk-8-jdk iproute2 procps libjemalloc1 # for kubespray, cassandra
+
