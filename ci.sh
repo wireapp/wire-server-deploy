9c2ecfb0aa66bf85bdf89905218156002e5ba0e7
Florian Klink <flokli@flokli.de>
2020-11-03 15:40:05 +0100

    ci: shellcheck, upload all binaries

--- ci.sh
+++ ci.sh
@@ -3,8 +3,13 @@
 # Build and install the environment
 nix-env -f default.nix -iA env
 
-# Build the debs
+mkdir -p static
+
+# Build the debs and publish them to static/debs
 mirror-bionic static/debs \
   python-apt aufs-tools apt-transport-https software-properties-common conntrack ipvsadm ipset curl rsync socat unzip e2fsprogs xfsprogs ebtables python3-minimal \
   openjdk-8-jdk iproute2 procps libjemalloc1 # for kubespray, cassandra
 
+# Copy the binaries to static/binaries
+mkdir -p static/binaries
+cp -R "$(nix-build --no-out-link -A pkgs.wire-binaries)/"* static/binaries/
