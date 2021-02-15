d2245de8790db1137ebdf41d80a2fb9a725eb70f
Gregor <gregor.jahn@wire.com>
2021-02-16 00:18:21 +0100

    [tf:k8s+dns-modules+env] Separate SPF record IPs from LB IPs
    THe LB usually does not send emails, thus using its IP(s) in an SPF record
    does not make sense. So, we need provide the node IP(s), because the SMTP
    server (deployed via Helm) will on one them.
    This change set makes it possible to define a list of IPv4 addresses
    specifically for the SPF record. If list is empty, no SPF record is being
    created.
    
    NOTE: it would have been nicer to generate the node IPs list similar to how
    it's done in kubernetes.inventory.tf, but Terraform had other plans. At the
    moment, I guess it's a bug that Tf wont see the indirect dependency. Otherwise
    it would not explain why it works having the logic moved into the k8s module.

--- terraform/modules/hetzner-kubernetes/outputs.tf
+++ terraform/modules/hetzner-kubernetes/outputs.tf
@@ -4,3 +4,12 @@ output "ips" {
       if contains( keys(machine.labels), "component-class.${local.LABEL_PREFIX}/node" )
     ]
 }
+
+# NOTE: the existence of this output feels indeed odd. What is generated here could and actually should
+#       be done on the outside since 'machines' is already exposed. See ./../../environment/kubernetes.dns.tf
+output "node_ips" {
+  value = [
+    for _, machine in hcloud_server.machines : machine.ipv4_address
+      if contains( keys(machine.labels), "component-class.${local.LABEL_PREFIX}/node" )
+  ]
+}
