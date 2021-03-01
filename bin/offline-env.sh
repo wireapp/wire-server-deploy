31b3bce87c954a2c9ae67009a333881206070510
Arian van Putten <arian.vanputten@gmail.com>
2021-03-01 17:31:22 +0100

    Fix offline-env alias

--- bin/offline-env.sh
+++ bin/offline-env.sh
@@ -8,4 +8,4 @@ export ZAUTH_CONTAINER
 
 WSD_CONTAINER=$(sudo docker load -i $SCRIPT_DIR/../containers-adminhost/container-wire-server-deploy.tgz | awk '{print $3}')
 
-alias d="sudo docker run -it -v --network=host -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent $HOME/.ssh:/root/.ssh -v $PWD:/wire-server-deploy $WSD_CONTAINER"
+alias d="sudo docker run -it --network=host -v ${SSH_AUTH_SOCK:-nonexistent}:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent -v $HOME/.ssh:/root/.ssh -v $PWD:/wire-server-deploy $WSD_CONTAINER"
