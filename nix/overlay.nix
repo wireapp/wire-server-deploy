7f6a517026720544f0c59eff2944a004aa750874
Akshay Mankar <itsakshaymankar@gmail.com>
2021-03-24 14:44:39 +0100

    nix: Ensure helmfile can access helm with plugins
    This causes helmfile to be rebuilt first time.

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -10,5 +10,7 @@ self: super: {
   pythonForAnsible = (self.python3.withPackages (_: self.ansible.requiredPythonModules));
 
   kubectl = self.callPackage ./pkgs/kubectl.nix { };
-
+  kubernetes-helm = super.wrapHelm super.kubernetes-helm {
+    plugins = with super.kubernetes-helmPlugins; [ helm-s3 helm-secrets helm-diff ];
+  };
 }
