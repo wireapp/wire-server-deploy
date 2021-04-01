8612f08b03741062565bd0b69e697989b051dbe5
Arian van Putten <arian.vanputten@gmail.com>
2021-04-01 11:05:58 +0200

    Fix ansible module dependencies
    What we were doing before was way too complicated. Ansible itself
    doesn't have any dependency on boto to function. The _target host_
    needs to have boto installed.
    
    By default the implicit localhost sets ansible_python_interpreter to
    ansible_playbook_python.  This used to work but stopped working. (Or
    maybe it never worked?) The python that ansible runs the playbooks as
    cant find boto.  I don't know what changed. Maybe how nix assembles
    python packages has changed.
    
    Instead, we configure ansible_python_interpreter to point to the
    environment we built with nix explicitly; which contains a python that
    has boto installed.
    
    This way boto can be found and the playbooks should succeed!
    
    We could use the same simplification in hegemony I think.

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,10 @@ COPY . /wire-server-deploy
 
 RUN apk add -u bash git
 
-RUN nix-env -f /wire-server-deploy/default.nix -iA env
+RUN nix-build /wire-server-deploy/default.nix -A env --out-link /.nix-env
 
 RUN rm -rf /wire-server-deploy
+
+ENV PATH="/.nix-env/bin:$PATH"
+ENV LOCALHOST_PYTHON="/.nix-env/bin/python"
+
--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,14 +1,4 @@
 self: super: {
-  ansible_with_libs = super.python3Packages.toPythonApplication (super.python3Packages.ansible.overridePythonAttrs (old: rec {
-    propagatedBuildInputs = old.propagatedBuildInputs or [] ++ [
-      super.python3Packages.boto
-      super.python3Packages.boto3
-      super.python3Packages.six
-    ];
-  }));
-
-  pythonForAnsible = (self.python3.withPackages (_: self.ansible.requiredPythonModules));
-
   kubectl = self.callPackage ./pkgs/kubectl.nix { };
   kubernetes-helm = super.wrapHelm super.kubernetes-helm {
     plugins = with super.kubernetes-helmPlugins; [ helm-s3 helm-secrets helm-diff ];
