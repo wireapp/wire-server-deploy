1d6f263d9a40dcf55c1f163de5a73f1dc1dfd176
Arian van Putten <arian.vanputten@gmail.com>
2021-05-07 15:20:09 +0200

    Try to fix gpg

--- nix/scripts/mirror-apt.sh
+++ nix/scripts/mirror-apt.sh
@@ -68,25 +68,20 @@ FOO
 
 aptly="aptly -config=${aptly_config} "
 
-echo "GPG is at $(whereis gpg)"
-
-# configure gpg to use a custom keyring, because aptly reads from it
-gpg="gpg --keyring=$GNUPGHOME/trustedkeys.gpg --no-default-keyring"
+echo "GPG is at $(which gpg)"
 
 # Import our signing key to our keyring
-echo -e "$GPG_PRIVATE_KEY" | $gpg --import
+echo -e "$GPG_PRIVATE_KEY" |  gpg --armor --keyring secring.gpg
 
-$gpg --list-keys
+echo "Printing the public key ids..."
+gpg --keyring secring.gpg --list-keys
 echo "Printing the secret key ids..."
-$gpg --list-secret-keys
-
-echo "What is in the GNUPGHOME"
-ls -la $GNUPGHOME
+gpg --keyring secring.gpg --list-secret-keys
 
 # import the ubuntu and docker signing keys
 # TODO: Do we want to pin these better? Verify them?
-curl 'https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x790bc7277767219c42c86f933b4fe6acc0b21f32' | $gpg --import
-curl https://download.docker.com/linux/ubuntu/gpg | $gpg --import
+curl 'https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x790bc7277767219c42c86f933b4fe6acc0b21f32' | gpg --keyring=trustedkeys.gpg --import
+curl https://download.docker.com/linux/ubuntu/gpg | gpg --keyring=trustedkeys.gpg --import
 
 $aptly mirror create -architectures=amd64 -filter="${packages_}" -filter-with-deps bionic http://de.archive.ubuntu.com/ubuntu/ bionic main universe
 $aptly mirror create -architectures=amd64 -filter="${packages_}" -filter-with-deps bionic-security http://de.archive.ubuntu.com/ubuntu/ bionic-security main universe
