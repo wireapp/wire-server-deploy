71a888555f5394c8a7ccede9f521bd381df4f4e9
Arian van Putten <arian.vanputten@gmail.com>
2021-05-07 14:58:55 +0200

    More debug

--- nix/scripts/mirror-apt.sh
+++ nix/scripts/mirror-apt.sh
@@ -68,6 +68,8 @@ FOO
 
 aptly="aptly -config=${aptly_config} "
 
+echo "GPG is at $(which gpg)"
+
 # configure gpg to use a custom keyring, because aptly reads from it
 gpg="gpg --keyring=$GNUPGHOME/trustedkeys.gpg --no-default-keyring"
 
