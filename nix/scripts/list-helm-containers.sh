7cfa84dfaa02e9f95cf5b9b33aeedd5332f02d54
Molly Miller <molly.miller@wire.com>
2022-03-24 10:59:59 +0100

    Hack (3).

--- nix/scripts/list-helm-containers.sh
+++ nix/scripts/list-helm-containers.sh
@@ -34,6 +34,7 @@ while IFS= read -r chart; do
     --set secrets.zrestSecret=emptyString \
     --set federate.dtls.tls.issuerRef=emptyString \
     --set federate.dtls.tls.certificate.labels=emptyString \
+    --set federate.dtls.tls.certificate.dnsNames=emptyString \
     $( [[ -f ./values/$(basename $chart)/prod-values.example.yaml ]] && echo "-f ./values/$(basename $chart)/prod-values.example.yaml" ) \
     $( [[ -f ./values/$(basename $chart)/prod-secrets.example.yaml ]] && echo "-f ./values/$(basename $chart)/prod-secrets.example.yaml" ) \
     | yq -r '..|.image? | select(.)' | optionally_complain | sort -u
