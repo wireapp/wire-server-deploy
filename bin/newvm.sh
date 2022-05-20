d2e238074a996d07b1cc21d7167ea2a78e265be0
Julia Longtin <julia.longtin@wire.com>
2022-05-20 10:36:56 +0100

    use better conditionals, and add cpu core argument.

--- bin/newvm.sh
+++ bin/newvm.sh
@@ -4,13 +4,16 @@ usage() { echo "Usage: $0 usage:" && grep ") \#" $0 && echo "        <VM name>" 
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-while getopts ":hqm:d:" o; do
+while getopts ":hqm:d:c:" o; do
     case "${o}" in
 	d) # set amount of disk, in gigabytes
 	    d=${OPTARG}
 	    ;;
 	m) # set amount of memory, in megabytes
 	    m=${OPTARG}
+	    ;;
+	c) # set amount of CPU cores.
+	    c=${OPTARG}
 	    ;;
 	q) # use qemu instead of kvm.
 	    q=1
@@ -55,8 +58,9 @@ fi
 
 echo "disk size = ${d} gigabytes"
 echo "memory = ${m} megabytes"
+echo "CPUs: ${c}"
 echo "hostname: $VM_NAME"
-if [ "$q" -eq 1 ]; then
+if [ ! -z "$q" ]; then
     echo "USE QEMU"
 fi
 
@@ -68,9 +72,10 @@ qemu-img create "$VM_NAME"/drive-c.img ${d}G
 sed -i "s/MEM=.*/MEM=${m}/" "$VM_NAME"/start_kvm.sh
 sed -i "s@CDROM=.*@CDROM=../ubuntu.iso@" "$VM_NAME"/start_kvm.sh
 sed -i "s/^eth1=/#eth1=/" "$VM_NAME"/start_kvm.sh
+sed -i "s/^CPUS=/CPUS=${c}/" "$VM_NAME"/start_kvm.sh
 sed -i 's/\(.*\)CURSES=.*/\1CURSES="-nographic -device sga"/' "$VM_NAME"/start_kvm.sh
 
-if [ "$q" -eq 1 ]; then
+if [ ! -z "$q" ]; then
     echo "forcing QEMU."
     sed -i "s=/usr/bin/kvm=/usr/bin/qemu-system-x86_64=" "$VM_NAME"/start_kvm.sh
 fi
