7656cd17b997acbf9e02d34e4ab7b464b4606b29
Florian Klink <flokli@flokli.de>
2022-06-14 20:36:19 +0200

    offline/ci.sh: bump images

--- offline/ci.sh
+++ offline/ci.sh
@@ -25,24 +25,24 @@ rm -r binaries
 
 function list-system-containers() {
 # These are manually updated with values from
-# https://github.com/kubernetes-sigs/kubespray/blob/release-2.15/roles/download/defaults/main.yml
+# ansible/roles-external/kubespray/roles/download/defaults/main.yml
 # TODO: Automate this. This is very wieldy :)
   cat <<EOF
-k8s.gcr.io/kube-apiserver:v1.19.7
-k8s.gcr.io/kube-controller-manager:v1.19.7
-k8s.gcr.io/kube-scheduler:v1.19.7
-k8s.gcr.io/kube-proxy:v1.19.7
-quay.io/coreos/etcd:v3.4.13
-quay.io/calico/node:v3.16.6
-quay.io/calico/cni:v3.16.6
-quay.io/calico/kube-controllers:v3.16.6
-docker.io/library/nginx:1.19
-k8s.gcr.io/coredns:1.7.0
-k8s.gcr.io/dns/k8s-dns-node-cache:1.16.0
-k8s.gcr.io/cpa/cluster-proportional-autoscaler-amd64:1.8.3
+k8s.gcr.io/kube-apiserver:v1.23.7
+k8s.gcr.io/kube-controller-manager:v1.23.7
+k8s.gcr.io/kube-scheduler:v1.23.7
+k8s.gcr.io/kube-proxy:v1.23.7
+quay.io/coreos/etcd:v3.5.3
+quay.io/calico/node:v3.22.3
+quay.io/calico/cni:v3.22.3
+quay.io/calico/kube-controllers:v3.22.3
+docker.io/library/nginx:1.21.4
+k8s.gcr.io/coredns:v1.8.6
+k8s.gcr.io/dns/k8s-dns-node-cache:1.21.1
+k8s.gcr.io/cpa/cluster-proportional-autoscaler-amd64:1.8.5
 k8s.gcr.io/pause:3.3
-docker.io/kubernetesui/dashboard-amd64:v2.1.0
-docker.io/kubernetesui/metrics-scraper:v1.0.6
+docker.io/kubernetesui/dashboard-amd64:v2.5.0
+docker.io/kubernetesui/metrics-scraper:v1.0.7
 EOF
 }
 
