f6de77b1451a97790a104e584b3d2ec187bbb5e1
Arthur Wolf <wolf.arthur@gmail.com>
2022-09-13 01:31:46 +0200

    adding export because of linting complaint appears unused. Verify use (or export if used externally).

--- kvmhelpers/GUESTBRIDGE-vars.sh
+++ kvmhelpers/GUESTBRIDGE-vars.sh
@@ -2,12 +2,20 @@
 
 # The bridge shared by all VMs. if you change this, you should probably reboot.
 BRIDGE=br1
+export BRIDGE
 
 # The paths to binaries we use for bringing up and down the interface.
 BRCTL="/sbin/brctl"
+export BRCTL
+
 IP="/sbin/ip"
+export IP
+
 IFCONFIG="/sbin/ifconfig"
+export IFCONFIG
+
 SUDO="/usr/bin/sudo"
+export SUDO
 
 # none of the rest of this should matter.
 
@@ -19,8 +27,13 @@ SUDO="/usr/bin/sudo"
 # 0 for true.
 # manage ISC DHCPD
 USEDHCP=1
+export USEDHCP
+
 # manage BIND
 USEDNS=1
+export USEDNS
 
 # Whether to assign an IP and use ufw to provide internet to the VMs using HOSTBRIDGE.
 HOSTROUTE=1
+export HOSTROUTE
+
