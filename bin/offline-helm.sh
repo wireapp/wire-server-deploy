ed58366f2b88bd4b068734e65c8ddab9d4b77b26
Sven Tennie <sven.tennie@wire.com>
2022-09-27 10:16:49 +0200

    Make helm install wire-server verbose

--- bin/offline-helm.sh
+++ bin/offline-helm.sh
@@ -9,7 +9,7 @@ helm upgrade --install --wait fake-aws ./charts/fake-aws --values ./values/fake-
 helm upgrade --install --wait demo-smtp ./charts/demo-smtp --values ./values/demo-smtp/prod-values.example.yaml
 helm upgrade --install --wait databases-ephemeral ./charts/databases-ephemeral --values ./values/databases-ephemeral/prod-values.example.yaml
 helm upgrade --install --wait reaper ./charts/reaper
-helm upgrade --install --wait wire-server ./charts/wire-server --values ./values/wire-server/prod-values.example.yaml --values ./values/wire-server/secrets.yaml
+helm --debug upgrade --install --wait wire-server ./charts/wire-server --values ./values/wire-server/prod-values.example.yaml --values ./values/wire-server/secrets.yaml
 helm upgrade --install --wait nginx-ingress-controller ./charts/nginx-ingress-controller
 
 # TODO: Requires certs; which we do not have in CI/CD at this point. future work =) (Would need cert-manager in offline package. That'd be neat)
