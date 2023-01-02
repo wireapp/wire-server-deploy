e5725590e09c7ecbfca4e9d5e0605b6d06cc0e8c
Amit Sagtani <asagtani06@gmail.com>
2023-01-02 23:08:22 +0530

    test ci with external dependency chart

--- offline/ci.sh
+++ offline/ci.sh
@@ -78,6 +78,7 @@ charts=(
   wire/fake-aws
   wire/minio-external
   wire/wire-server
+  wire/inbucket
   # local-path-provisioner
   # TODO: uncomment once its dependencies are pinned!
   wire/sftd
