c25c564cf8deaddcdf01e99852a25103f7297c4c
Rohan Mahy <rohan.mahy@wire.com>
2023-01-23 11:43:07 -0800

    Initial version of offline/demo-onebox.md with matching script

---
+++ bin/install-demo.sh
@@ -0,0 +1,247 @@
+#!/bin/bash 
+# install-demo script
+
+echo "***** Reading variables from site.ini ******"
+source ~/Wire-Server/site.ini
+
+echo "***** Installing packages ******"
+sudo systemctl disable systemd-resolved
+sudo apt install -y dnsmasq ufw qemu-kvm qemu-utils sgabios bridge-utils docker.io
+sudo systemctl stop systemd-resolved
+
+echo "****** Configuring networking on host ******"
+# service DNS requests locally via dnsmasq
+sudo bash -c 'echo "listen-address=127.0.0.53" > /etc/dnsmasq.d/00-lo-systemd-resolvconf'
+sudo bash -c 'echo "no-resolv" >> /etc/dnsmasq.d/00-lo-systemd-resolvconf'
+sudo bash -c 'echo "server=8.8.8.8" >> /etc/dnsmasq.d/00-lo-systemd-resolvconf'
+sudo service dnsmasq restart
+
+# configure firewall
+sudo ufw allow 22/tcp
+sudo ufw allow from 172.16.0.0/24 proto udp to any port 53
+sudo ufw allow from 127.0.0.0/24 proto udp to any port 53
+sudo ufw allow in on br0 from any proto udp to any port 67
+sudo ufw allow from 172.16.0.0/24 proto tcp to 172.16.0.1 port 8008
+sudo ufw enable
+
+# create bridge interface
+sudo brctl addbr br0
+sudo ifconfig br0 172.16.0.1 netmask 255.255.255.0 up
+
+# setup dhcp for the kvms
+sudo bash -c 'echo "listen-address=172.16.0.1" > /etc/dnsmasq.d/10-br0-dhcp'
+sudo bash -c 'echo "dhcp-range=172.16.0.2,172.16.0.127,10m" >> /etc/dnsmasq.d/10-br0-dhcp'
+sudo service dnsmasq restart
+
+# ip forwarding
+sudo sed -i "s/.*net.ipv4.ip_forward.*/net.ipv4.ip_forward=1/" /etc/sysctl.conf 
+sudo sysctl -p
+
+# masquerading
+OUTBOUNDINTERFACE=`ip ro | grep ^default | egrep -o 'en[ps0-9a-f]+'`
+sudo sed -i 's/.*DEFAULT_FORWARD_POLICY=.*/DEFAULT_FORWARD_POLICY="ACCEPT"/' /etc/default/ufw
+sudo sed -i "1i *nat\n:POSTROUTING ACCEPT [0:0]\n-A POSTROUTING -s 172.16.0.0/24 -o $OUTBOUNDINTERFACE -j MASQUERADE\nCOMMIT" /etc/ufw/before.rules
+sudo service ufw restart
+
+echo "****** Fetching Wire-Server artifact ******"
+wget "${ARTIFACT_URL}"
+
+echo "****** Extracting Wire-Server artifact ******"
+tar -xzf ../wire-server-deploy-static-*.tgz
+tar -xf debs.tar
+sudo dpkg -i debs/public/pool/main/d/docker-ce/docker-ce-cli_*.deb
+sudo dpkg -i debs/public/pool/main/c/containerd.io/containerd.io_*.deb 
+sudo dpkg -i debs/public/pool/main/d/docker-ce/docker-ce_*.deb
+sudo dpkg --configure -a
+
+echo "****** Copy deployment scripts ******"
+cp -a wire-server-deploy/kvmhelpers/ ./
+cp -a wire-server-deploy/bin/newvm.sh ./bin
+cp -a wire-server-deploy/ansible/setup-offline-sources.yml ./ansible # see https://github.com/wireapp/wire-server-deploy/blob/kvm_support/offline/docs.md#workaround-old-debian-key 
+cp wire-server-deploy/offline/kvm-demo-hosts.ini hosts.ini
+cp wire-server-deploy/offline/kvm-demo-host.json .
+chmod 550 ./bin/newvm.sh ./bin/autoinstall ./kvmhelpers/*.sh
+
+echo "****** Download Ubuntu Server CD ******"
+curl https://releases.ubuntu.com/18.04.3/ubuntu-18.04.3-live-server-amd64.iso -o ubuntu.iso
+# TODO: update to 18.04.6 by updating this script and autoinstall script for
+#         correct location of linux and initrd
+#curl https://releases.ubuntu.com/18.04.3/ubuntu-18.04.3-live-server-amd64.iso -o ubuntu.iso
+# WARNING: check that the instructions still work for this image, or find the mini.iso.
+sudo mkdir -p /mnt/iso
+sudo mount -r ubuntu.iso /mnt/iso
+cp /mnt/iso/initrd.gz .
+gunzip initrd.gz
+
+echo "****** Setup autoinstall web server ******"
+mkdir -p ~/Wire-Server/autoinstall/d-i/bionic
+cp wire-server-deploy/offline/preseed-template.cfg autoinstall/
+(cd ~/Wire-Server/autoinstall && screen -S autoinstall_web -d -m 'python3 -m http.server 8008')
+
+echo "****** Configure KVM ******"
+# add demo to kvm group
+sudo usermod -a -G kvm demo
+
+# assign each host a static DHCP address
+sudo bash -c 'echo "dhcp-host=assethost,172.16.0.128,10h" > /etc/dnsmasq.d/20-hosts'
+sudo bash -c 'echo "dhcp-host=kubenode1,172.16.0.129,10h" >> /etc/dnsmasq.d/20-hosts'
+sudo bash -c 'echo "dhcp-host=kubenode2,172.16.0.130,10h" >> /etc/dnsmasq.d/20-hosts'
+sudo bash -c 'echo "dhcp-host=kubenode3,172.16.0.131,10h" >> /etc/dnsmasq.d/20-hosts'
+sudo bash -c 'echo "dhcp-host=ansnode1,172.16.0.132,10h" >> /etc/dnsmasq.d/20-hosts'
+sudo bash -c 'echo "dhcp-host=ansnode2,172.16.0.133,10h" >> /etc/dnsmasq.d/20-hosts'
+sudo bash -c 'echo "dhcp-host=ansnode3,172.16.0.134,10h" >> /etc/dnsmasq.d/20-hosts'
+sudo service dnsmasq restart
+
+# TODO: walk through kvm-demo-host.json instead
+# sudo python3 setup-dhcp kvm-demo-host.json
+#    read/parse the json file
+#    for each host, print(f"dhcp-host={nodename},172.16.0.{ip},10h") into /etc/dnsmasq.d/20-hosts
+
+echo "****** Create KVMs and Install ubuntu"
+
+# Create kvm directories manually
+./bin/newvm.sh -d 40 -m 1024 -c 1 -t tap_asset -M 0 assethost
+./bin/newvm.sh -d 80 -m 8192 -c 6 -t tap_kube1 -M 1 kubenode1
+./bin/newvm.sh -d 80 -m 8192 -c 6 -t tap_kube2 -M 2 kubenode2
+./bin/newvm.sh -d 80 -m 8192 -c 6 -t tap_kube3 -M 3 kubenode3
+./bin/newvm.sh -d 80 -m 8192 -c 6 -t tap_ans1 -M 4 ansnode1
+./bin/newvm.sh -d 80 -m 8192 -c 6 -t tap_ans2 -M 5 ansnode2
+./bin/newvm.sh -d 80 -m 8192 -c 6 -t tap_ans3 -M 6 ansnode3
+
+# Run the autoinstall script for each node
+cd assethost
+../bin/autoinstall -d 40 -m 1024 -c 1 -t tap_asset -M 0
+cd ../kubenode1
+../bin/autoinstall -d 80 -m 8192 -c 6 -t tap_kube1 -M 1
+cd ../kubenode2
+../bin/autoinstall -d 80 -m 8192 -c 6 -t tap_kube2 -M 2
+cd ../kubenode3
+../bin/autoinstall -d 80 -m 8192 -c 6 -t tap_kube3 -M 3
+cd ../ansnode1
+../bin/autoinstall -d 80 -m 8192 -c 6 -t tap_ans1 -M 4
+cd ../ansnode2
+../bin/autoinstall -d 80 -m 8192 -c 6 -t tap_ans2 -M 5
+cd ../ansnode3
+../bin/autoinstall -d 80 -m 8192 -c 6 -t tap_ans3 -M 6
+cd ..
+
+# Backup freshly installed kvms
+cp -a assethost assethost-new
+cp -a ansnode1 ansnode1-new
+cp -a ansnode2 ansnode2-new
+cp -a ansnode3 ansnode3-new
+cp -a kubenode1 kubenode1-new
+cp -a kubenode2 kubenode2-new
+cp -a kubenode3 kubenode3-new
+
+# TODO
+#for each one
+#for host in hostparams:
+#  (nodename, short, cpus, mem, disk, ip, mac_seq, nets) = host
+#  tap_prefix = 'tap_' + short
+#  setup_dhcp(nodename, "172.16.0." + ip)
+#  create_kvm(nodename, disk, cpus, mem, mac_seq, tap_prefix)
+#  install_ubuntu(nodename, cpus, mem, mac_seq, tap_prefix)
+#  backup_ubuntu(nodename)
+#  start_in_detached_screen(nodename)
+
+# create kvms 
+# f"bin/newvm -d {disk} -m {mem} -c {cpus} -t {tap_prefix} -M {mac_seq} {nodename}"
+# autoinstall kvms
+# f"(cd {nodename} && ../bin/autoinstall -m {mem} -c {cpus} -t {tap_prefix} -M {mac_seq})"
+# backup freshly installed kvm
+# cp -a {nodename} {nodename}-new
+# start each kvm in it's own detached screen
+# f"cd {nodename} && screen -S {nodename} -d -m ./start_kvm.sh"
+
+echo "****** Stop autoinstall web server ******"
+# gotta be a better way
+kill -KILL `screen -list | grep '.autoinstall_web' | cut -d . -f 1 | egrep -o '[0-9]+'`
+screen -wipe
+
+echo "****** Start each KVM in it's own detached screen ******"
+cd assethost    && screen -S assethost -d -m ./start_kvm.sh
+cd ../kubehost1 && screen -S kubehost1 -d -m ./start_kvm.sh
+cd ../kubehost2 && screen -S kubehost2 -d -m ./start_kvm.sh
+cd ../kubehost3 && screen -S kubehost3 -d -m ./start_kvm.sh
+cd ../anshost1  && screen -S anshost1  -d -m ./start_kvm.sh
+cd ../anshost2  && screen -S anshost2  -d -m ./start_kvm.sh
+cd ../anshost3  && screen -S anshost3  -d -m ./start_kvm.sh
+cd ..
+
+echo "****** More networking ******"
+
+
+# TODO ??
+EXTERNALIPADDRESS=`ip ro | grep ^default | egrep -o 'via [0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | egrep -o '[0-9.]+'`
+# get PUBLICIPADDRESS from site.ini and compare
+PUBLICIPADDRESS=$EXTERNALIPADDRESS
+IP_IS_PUBLIC=1
+
+
+KUBENODE1IP=172.16.0.129
+RESTUND01IP=172.16.0.132
+#  TODO: fetch the KUBENODE1IP from nodes.params
+#KUBENODE1IP=``
+#RESTUND01IP=``
+
+if [ $IP_IS_PUBLIC = 1 ]; then
+  sudo iptables -t nat -A PREROUTING -d $PUBLICIPADDRESS -i $OUTBOUNDINTERFACE -p tcp --dport 80 -j DNAT --to-destination $KUBENODE1IP:31772
+  sudo iptables -t nat -A PREROUTING -d $PUBLICIPADDRESS -i $OUTBOUNDINTERFACE -p tcp --dport 443 -j DNAT --to-destination $KUBENODE1IP:31773
+else
+  sudo iptables -t nat -A PREROUTING -i $OUTBOUNDINTERFACE -p tcp --dport 80 -j DNAT --to-destination $KUBENODE1IP:31772
+  sudo iptables -t nat -A PREROUTING -i $OUTBOUNDINTERFACE -p tcp --dport 443 -j DNAT --to-destination $KUBENODE1IP:31773
+fi
+sudo ufw allow in on $OUTBOUNDINTERFACE proto tcp to any port 443
+sudo ufw allow in on $OUTBOUNDINTERFACE proto tcp to any port 80
+# Julia: do I need these?
+sudo ufw allow in on $OUTBOUNDINTERFACE proto udp to any port 3478
+sudo ufw allow in on $OUTBOUNDINTERFACE proto tcp to any port 3478
+sudo ufw allow in on $OUTBOUNDINTERFACE proto tcp to any port 5349
+
+# TODO: make sure to add the above iptables rules to /etc/ufw/before.rules, so they persist after a reboot
+sudo iptables -t nat -A PREROUTING -i br0 -d $PUBLICIPADDRESS -p tcp --dport 80 -j DNAT --to-destination $KUBENODE1IP:31772
+sudo iptables -t nat -A PREROUTING -i br0 -d $PUBLICIPADDRESS -p tcp --dport 443 -j DNAT --to-destination $KUBENODE1IP:31773
+
+sudo iptables -t nat -A PREROUTING -d $PUBLICIPADDRESS -i $OUTBOUNDINTERFACE -p udp --dport 3478 -j DNAT --to-destination $RESTUND01IP:3478
+sudo iptables -t nat -A PREROUTING -d $PUBLICIPADDRESS -i $OUTBOUNDINTERFACE -p tcp --dport 3478 -j DNAT --to-destination $RESTUND01IP:3478
+sudo iptables -t nat -A PREROUTING -d $PUBLICIPADDRESS -i $OUTBOUNDINTERFACE -p tcp --dport 5349 -j DNAT --to-destination $RESTUND01IP:5349
+sudo iptables -t nat -A PREROUTING -d $PUBLICIPADDRESS -i $OUTBOUNDINTERFACE -p udp -m udp --dport 32768:60999 -j DNAT --to-destination $RESTUND01IP
+
+#ERROR: after you install restund, the restund firewall will fail to start.
+#
+#delete the outbound rule to 172.16.0.0/12
+#
+#sudo ufw status numbered
+#sudo ufw delete <right number>
+
+echo "****** Install certmanager ******"
+wget https://charts.jetstack.io/charts/cert-manager-v1.9.1.tgz
+mkdir tmp
+(cd tmp && tar -xzf ../cert-manager-*.tgz)
+mv tmp/cert-manager/ charts/
+rm -rf tmp
+sed -i "s/  useCertManager: false/  useCertManager: true/" values/nginx-ingress-services/values.yaml
+sed -i "s/(  certmasterEmail:)/\1 ${INSTALL_EMAIL}/" values/nginx-ingress-services/values.yaml
+sed -i "s/example.com/${WIRE_DOMAIN}/" values/nginx-ingress-services/values.yaml
+
+
+echo "****** Create the host.ini file ******"
+cp wire-server-deploy/offline/kvm-demo-hosts.ini ansible/inventory/offline/hosts.ini
+sed -i "s/EXTERNAL_IP/${PUBLICIPADDRESS}/g" ansible/inventory/offline/hosts.ini
+sed -i "s/DOMAIN/${WIRE_DOMAIN}/g" ansible/inventory/offline/hosts.ini
+mv ansible/inventory/offline/99-static ansible/inventory/offline/orig.99-static
+
+echo "****** Fix restund configuration ******"
+# TODO:
+
+
+echo "****** First phase of the install is complete ******"
+echo
+echo "Please run the following commands:"
+echo "source ./bin/offline-env.sh"
+echo "./bin/offline-secrets.sh"
+echo
+echo "then follow instructions from:"
+echo "  https://github.com/wireapp/wire-server-deploy/blob/master/offline/docs.md#deploying-with-ansible"
+
---
+++ offline/demo-onebox.md
@@ -0,0 +1,138 @@
+# Scope
+
+This document gives exact instructions for performing an offline installation of Wire on a single VM from Hetzner. it uses the KVM virtual machine system to create all of the required virtual machines.
+
+## Use the hetzner robot console to create a new server.
+
+Select Ubuntu 18.04 or Ubuntu 20.04 on an ax101 dedicated server.
+
+If not using Hetzner, for reference, the specs of the ax101 server are:
+
+* AMD Ryzen™ 9 5950X
+* 128 GB DDR4 ECC RAM
+* 2 x 3.84 TB NVMe SSD Datacenter Edition (software RAID 1) 
+* 1 GBit/s port
+
+In our example, the returned IP when creating the server was: 65.21.197.76
+
+## Pre-requisites
+
+On your local machine if you don't have an ed25519 ssh key yet, generate one:
+
+```
+ssh-keygen -t ed25519
+```
+
+Set a variable with the IP address of the host you are setting up:
+
+```
+HOST_IP=1.2.3.4
+```
+
+Add your local ssh public key as an authorized key on the host (if not already done).
+Either do via the robot or (if you have a root password) with these commands:
+
+```
+scp ~/.ssh/id_ed25519.pub "root@{HOST_IP}:"
+ssh "root@{HOST_IP}" '/bin/bash -c "mkdir -p ~/.ssh/ && chmod 700 ~/.ssh/ && cat id_ed25519.pub >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys && rm ~/id_ed25519.pub"'
+```
+
+Create a `site.ini` file locally. Populate it with the demo installation's actual
+domain name, public IP address, the installer's email address (used for certbot),
+and the URL where the wire-server can be downloaded:
+
+```
+echo WIRE_DOMAIN=example.com > site.ini
+echo PUBLICIPADDRESS=1.2.3.4 >> site.ini
+echo INSTALLER_EMAIL=alice.admin@provider.net >> site.ini
+echo ARTIFACT_URL=https://s3-eu-west-1.amazonaws.com/public.wire.com/artifacts/wire-server-deploy-static-03fad4ff6d9a67eb56668fb259a0c1571cabcac4.tgz >> site.ini
+```
+
+## Update the host packages and create a demo user
+
+Login to the host as root using your key instead of a password:
+
+```
+ssh -i ~/.ssh/id_ed25519 "root@${HOST_IP}" -o serveraliveinterval=60
+```
+
+Run these commands on the host (as root):
+
+```
+apt update && apt upgrade -y 
+apt install -y tmate fail2ban
+sed -i -re 's/^(PasswordAuthentication)(.+)/\1 no/' /etc/ssh/sshd_config
+sed -i -re 's/^(UsePAM)(.+)/\1 no/' /etc/ssh/sshd_config
+sed -i -re 's/^(PermitRootLogin)(.+)/\1 prohibit-password/' /etc/ssh/sshd_config
+service ssh restart
+adduser --disabled-password --gecos "" demo
+mkdir ~demo/.ssh ~demo/Wire-Server
+cp ~/.ssh/authorized_keys /home/demo/.ssh/
+chown -R demo:demo ~demo/
+chmod 700 ~demo/.ssh
+echo "demo ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/10-demo_user
+chmod 440 /etc/sudoers.d/10-demo_user
+reboot
+```
+
+## Download the deployment scripts and prepare and install all the kvms
+
+Copy the site.ini file to the host:
+
+```
+scp -i ~/.ssh/id_ed25519 site.ini "demo@${HOST_IP}:Wire-Server/"
+```
+
+Login to the host as the demo user:
+
+```
+ssh -i ~/.ssh/id_ed25519 "demo@${HOST_IP}" -o serveraliveinterval=60
+```
+
+Run these commands on the host (as demo):
+
+```
+sed -i -re 's/^(PermitRootLogin)(.+)/\1 no/' /etc/ssh/sshd_config
+service ssh restart
+sudo apt install -y git
+cd ~/Wire-Server
+git clone https://github.com/wireapp/wire-server-deploy.git
+wire-server-deploy/bin/install-demo
+```
+
+
+
+### From this point:
+
+switch to docs.md.
+
+skip down to 'Making tooling available in your environment'
+
+### ERROR: after you install restund, the restund firewall will fail to start.
+
+delete the outbound rule to 172.16.0.0/12
+```
+sudo ufw status numbered
+sudo ufw delete <right number>
+```
+
+#### enable the ports colocated services run on:
+cassandra:
+```
+sudo ufw allow 9042/tcp
+sudo ufw allow 9160/tcp
+sudo ufw allow 7000/tcp
+sudo ufw allow 7199/tcp
+```
+
+elasticsearch:
+```
+sudo ufw allow 9300/tcp
+sudo ufw allow 9200/tcp
+```
+
+minio:
+```
+sudo ufw allow 9000/tcp
+sudo ufw allow 9092/tcp
+```
