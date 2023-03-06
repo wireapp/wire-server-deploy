8719800c76b4f9b6d39f17d79a9547631c0c4d4e
Zebot <zebot@users.noreply.github.com>
2023-03-06 20:01:41 +0000

    Update wire-server charts to version 4.34.0

--- offline/ci.sh
+++ offline/ci.sh
@@ -97,7 +97,7 @@ helm repo add wire https://s3-eu-west-1.amazonaws.com/public.wire.com/charts
 helm repo update
 
 # wire_version=$(helm show chart wire/wire-server | yq -r .version)
-wire_version="4.33.0"
+wire_version="4.34.0"
 
 # Download zauth; as it's needed to generate certificates
 echo "quay.io/wire/zauth:$wire_version" | create-container-dump containers-adminhost
