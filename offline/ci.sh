6b4a8550b36c8b4ee16c7de4146c5ed690f00783
Sven Tennie <sven.tennie@wire.com>
2023-03-16 16:37:44 +0100

    Frida: Upgrade to 4.34.0-hotfix.4

--- offline/ci.sh
+++ offline/ci.sh
@@ -94,10 +94,11 @@ HELM_HOME=$(mktemp -d)
 export HELM_HOME
 
 helm repo add wire https://s3-eu-west-1.amazonaws.com/public.wire.com/charts
+helm repo add wire-develop https://s3-eu-west-1.amazonaws.com/public.wire.com/charts-develop
 helm repo update
 
 # wire_version=$(helm show chart wire/wire-server | yq -r .version)
-wire_version="4.34.0"
+wire_version="4.34.0-hotfix.4"
 
 # Download zauth; as it's needed to generate certificates
 echo "quay.io/wire/zauth:$wire_version" | create-container-dump containers-adminhost
