9b12d506e162dcbc1d619458f996ce2dc87f4e43
Sven Tennie <sven.tennie@wire.com>
2023-04-13 17:28:46 +0200

    Don't fail on failing helm deployment

--- bin/offline-helm.sh
+++ bin/offline-helm.sh
@@ -9,7 +9,7 @@ helm upgrade --install --wait fake-aws ./charts/fake-aws --values ./values/fake-
 helm upgrade --install --wait demo-smtp ./charts/demo-smtp --values ./values/demo-smtp/prod-values.example.yaml
 helm upgrade --install --wait databases-ephemeral ./charts/databases-ephemeral --values ./values/databases-ephemeral/prod-values.example.yaml
 helm upgrade --install --wait reaper ./charts/reaper
-helm upgrade --debug --install --wait --timeout 10m wire-server ./charts/wire-server --values ./values/wire-server/prod-values.example.yaml --values ./values/wire-server/secrets.yaml
+helm upgrade --debug --install --wait --timeout 10m wire-server ./charts/wire-server --values ./values/wire-server/prod-values.example.yaml --values ./values/wire-server/secrets.yaml || true
 echo "wire-server EVENTS"
 kubectl get events
 kubectl get -n wire events
