5d33089c72725080452ff4ced96d4c6c7f53a82a
Sven Tennie <sven.tennie@wire.com>
2023-04-13 09:12:36 +0200

    Add --debug to wire-server helm-ing

--- bin/offline-helm.sh
+++ bin/offline-helm.sh
@@ -9,7 +9,7 @@ helm upgrade --install --wait fake-aws ./charts/fake-aws --values ./values/fake-
 helm upgrade --install --wait demo-smtp ./charts/demo-smtp --values ./values/demo-smtp/prod-values.example.yaml
 helm upgrade --install --wait databases-ephemeral ./charts/databases-ephemeral --values ./values/databases-ephemeral/prod-values.example.yaml
 helm upgrade --install --wait reaper ./charts/reaper
-helm upgrade --install --wait wire-server ./charts/wire-server --values ./values/wire-server/prod-values.example.yaml --values ./values/wire-server/secrets.yaml
+helm upgrade --debug --install --wait wire-server ./charts/wire-server --values ./values/wire-server/prod-values.example.yaml --values ./values/wire-server/secrets.yaml
 helm upgrade --install --wait ingress-nginx-controller ./charts/ingress-nginx-controller --values ./values/ingress-nginx-controller/prod-values.example.yaml
 
 # TODO: Requires certs; which we do not have in CI/CD at this point. future work =) (Would need cert-manager in offline package. That'd be neat)
