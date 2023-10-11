85fe7dfa60b1ed4c59cee06ba41063bd44a447ed
Vedran Ivanković <33936733+Veki301@users.noreply.github.com>
2023-10-11 10:41:38 +0200

    make restund pass pass helm templating

--- nix/scripts/list-helm-containers.sh
+++ nix/scripts/list-helm-containers.sh
@@ -31,9 +31,6 @@ while IFS= read -r chart; do
   echo "Running helm template on chart ${chart}…" >&2
   # The image values are left as-is
   helm template "$chart" \
-    --set secrets.zrestSecret=emptyString \
-    --set federate.dtls.tls.key=emptyString \
-    --set federate.dtls.tls.crt=emptyString \
     $( [[ -f ./values/$(basename $chart)/prod-values.example.yaml ]] && echo "-f ./values/$(basename $chart)/prod-values.example.yaml" ) \
     $( [[ -f ./values/$(basename $chart)/prod-secrets.example.yaml ]] && echo "-f ./values/$(basename $chart)/prod-secrets.example.yaml" ) \
     | yq -r '..|.image? | select(.)' | optionally_complain | sort -u
