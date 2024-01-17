2f656177dd335ac01cbacb5f3c6c0653f39c3388
Jan Schumacher <jan.schumacher@wire.com>
2024-01-17 17:52:18 +0100

    WPB-2324 introduce more automation for dedicated Hetzner server deployments prior to kubespray upgrades

--- bin/offline-vm-setup.sh
+++ bin/offline-vm-setup.sh
@@ -1,26 +1,101 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
+set -Eeuo pipefail
 
-nocloud_basedir=/home/demo/wire-server-deploy/nocloud
+if [[ $EUID -eq 0 ]]; then
+  echo "Please don't run me as root" 1>&2
+  exit 1
+fi
+
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+usage() {
+  cat <<EOF
+Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [--deploy-vm vmname]
+
+Non-interactive script for deploying standard set of Ubuntu Server VMs on a single dedicated Hetzner server.
+Script will create VMs with a sudo user "demo" and PW auth disabled.
+For SSH access, it'll use the 1st key found in the local user's .ssh/authorized_keys.
+If no key can be found, it will interactively ask for a key (and accept any input, so be careful).
+
+Default mode with no arguments creates seven libvirt VMs using cloud-init:
+ * assethost
+ * kubenode1
+ * kubenode2
+ * kubenode3
+ * ansnode1
+ * ansnode2
+ * ansnode3
+
+Available options:
+-h, --help          Print this help and exit
+-v, --verbose       Print script debug info
+--deploy-vm vmname  Deploys a single Ubuntu VM
+EOF
+  exit
+}
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  pkill -f "http.server"
+  rm -rf "$DEPLOY_DIR"/nocloud/*
+}
+
+msg() {
+  echo >&2 -e "${1-}"
+}
+
+die() {
+  local msg=$1
+  local code=${2-1} # default exit status 1
+  msg "$msg"
+  exit "$code"
+}
+
+parse_params() {
+  while :; do
+    case "${1-}" in
+    -h | --help) usage ;;
+    -v | --verbose) set -x ;;
+    --deploy-vm) DEPLOY_SINGLE_VM=1 ;;
+    -?*) die "Unknown option: $1" ;;
+    *) break ;;
+    esac
+    shift
+  done
+  return 0
+}
+
+parse_params "$@"
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+DEPLOY_DIR="$(cd "$SCRIPT_DIR/../" && pwd)"
+NOCLOUD_DIR=$DEPLOY_DIR/nocloud
+
+if [ ! -d "$NOCLOUD_DIR" ]; then
+  mkdir -p "$NOCLOUD_DIR"
+fi
+
+if [[ -n "${DEPLOY_SINGLE_VM-}" ]]; then
+  VM_NAME="$2"
+else
+  VM_NAME="assethost kubenode1 kubenode2 kubenode3 ansnode1 ansnode2 ansnode3"
+fi
+
+nohup python3 -m http.server 3003 -d "$NOCLOUD_DIR" &
+
+if [[ -f ~/.ssh/authorized_keys && -s ~/.ssh/authorized_keys ]]; then
+  SSHKEY=$(head -n 1 ~/.ssh/authorized_keys)
+  echo "Including local SSH key ""$SSHKEY"" for VM deployment"
+else
+  read -r -p "No local SSH keys for current user ""$USER"" found; please enter a key now: " SSHKEY
+fi
 
 prepare_config() {
-    # Run
-    # export OFFLINE_PASSWORD="$(mkpasswd)"
-    # to set the hashed password
-    set -u
-    # shellcheck disable=SC2153
-    offline_username=$OFFLINE_USERNAME
-    # shellcheck disable=SC2153
-    offline_password=$OFFLINE_PASSWORD
-    set +u
-
-    name="$1"
-    d=$nocloud_basedir/$name
-    mkdir -p "$d"
-    touch "$d"/vendor-data
-    touch "$d"/meta-data
-    cat >"$d/user-data"<<EOF
+  VM_DIR=$NOCLOUD_DIR/$VM
+  mkdir -p "$VM_DIR"
+  touch "$VM_DIR"/{vendor-data,meta-data}
+  cat >"$VM_DIR/user-data"<<EOF
 #cloud-config
 autoinstall:
   version: 1
@@ -30,67 +105,43 @@ autoinstall:
     ethernets:
       enp1s0:
         dhcp4: yes
-  identity:
-    hostname: $name
-    password: $offline_password
-    username: $offline_username
   ssh:
-    install-server: yes
+    allow-pw: false
+    install-server: true
+  apt:
+    fallback: offline-install
+  user-data:
+    hostname: $VM
+    users:
+    - default
+    - name: demo
+      groups: sudo
+      sudo: ALL=(ALL) NOPASSWD:ALL
+      shell: /bin/bash
+      ssh_authorized_keys: 
+        - $SSHKEY
 EOF
 }
 
-create_assethost () {
-    name="$1"
+create_vm () {
+  prepare_config "$VM"
 
-    prepare_config "$name"
-
-    # if you want to run the installation manually remove the `--noautoconsole` flag and the ds= part from `--extra-args`
-    sudo virt-install \
-       --name "$name" \
-        --ram 8192 \
-       --disk path=/var/kvm/images/"$name".img,size=100  \
-       --vcpus 4 \
-       --network bridge=br0 \
-       --graphics none \
-       --osinfo detect=on,require=off \
-       --noautoconsole \
-       --location /home/demo/wire-server-deploy/ubuntu.iso,kernel=casper/vmlinuz,initrd=casper/initrd \
-       --extra-args "console=ttyS0,115200n8 autoinstall ds=nocloud-net;s=http://172.16.0.1:3003/$name"
+  sudo virt-install \
+    --name "$VM" \
+    --ram 8192 \
+    --disk path=/var/lib/libvirt/images/"$VM".qcow2,size=100  \
+    --vcpus 4 \
+    --network bridge=virbr0 \
+    --graphics none \
+    --osinfo detect=on,require=off \
+    --noautoconsole \
+    --location "$DEPLOY_DIR"/ubuntu.iso,kernel=casper/vmlinuz,initrd=casper/initrd \
+    --extra-args "console=ttyS0,115200n8 autoinstall ds=nocloud-net;s=http://192.168.122.1:3003/$VM"
 }
 
-create_node () {
-    name="$1"
-
-    prepare_config "$name"
-
-    # if you want to run the installation manually remove the `--noautoconsole` flag and the ds= part from `--extra-args`
-    sudo virt-install \
-        --name "$name" \
-        --ram 8192 \
-        --disk path=/var/kvm/images/"$name".img,size=100 \
-        --vcpus 6 \
-        --network bridge=br0 \
-        --graphics none \
-        --osinfo detect=on,require=off \
-        --noautoconsole \
-        --location /home/demo/wire-server-deploy/ubuntu.iso,kernel=casper/vmlinuz,initrd=casper/initrd \
-        --extra-args "console=ttyS0,115200n8 autoinstall ds=nocloud-net;s=http://172.16.0.1:3003/$name"
-}
-
-if [ "$1" = "serve_nocloud" ]; then
-    mkdir -p "$nocloud_basedir"
-    cd "$nocloud_basedir"
-    python3 -m http.server 3003
-fi
-
-if [ "$1" = "create_node" ]; then
-    set -u
-    name="$2"
-    create_node "$name"
-fi
-
-if [ "$1" = "create_assethost" ]; then
-    set -u
-    name="$2"
-    create_assethost "$name"
-fi
+for VM in $VM_NAME; do
+  set -u
+  msg "Creating VM $VM ..."
+  create_vm "$VM"
+  sleep 20
+done
