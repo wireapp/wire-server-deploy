0349f700a87695285a1affaa9fa892004c48bad1
Amit Sagtani <asagtani06@gmail.com>
2024-01-29 16:03:32 +0100

    debug pods status before installing wire-server

--- bin/offline-helm.sh
+++ bin/offline-helm.sh
@@ -11,6 +11,7 @@ helm upgrade --install --wait demo-smtp ./charts/demo-smtp --values ./values/dem
 helm upgrade --install --wait rabbitmq ./charts/rabbitmq --values ./values/rabbitmq/prod-values.example.yaml --values ./values/rabbitmq/prod-secrets.example.yaml
 helm upgrade --install --wait databases-ephemeral ./charts/databases-ephemeral --values ./values/databases-ephemeral/prod-values.example.yaml
 helm upgrade --install --wait reaper ./charts/reaper
+kubectl get pods --all-namespaces -o wide
 helm upgrade --install --wait --timeout=15m0s wire-server ./charts/wire-server --values ./values/wire-server/prod-values.example.yaml --values ./values/wire-server/secrets.yaml
 echo "Printing all pods status"
 kubectl get pods --all-namespaces -o wide
