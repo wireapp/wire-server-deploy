4fd308746a9462b7a40d5dbbb37168441ac05bcc
Julia Longtin <julia.longtin@wire.com>
2024-01-31 12:20:17 +0000

    Update offline/ci.sh

--- offline/ci.sh
+++ offline/ci.sh
@@ -154,6 +154,6 @@ tar cf containers-helm.tar containers-helm
 
 echo "docker_ubuntu_repo_repokey: '${fingerprint}'" > ansible/inventory/offline/group_vars/all/key.yml
 
-tar czf assets.tgz debs.tar binaries.tar containers-adminhost containers-helm.tar containers-oth
+tar czf assets.tgz debs.tar binaries.tar containers-adminhost containers-helm.tar containers-other.tar containers-system.tar ansible charts values bin
 
 echo "Done"
