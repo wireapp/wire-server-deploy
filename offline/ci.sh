5b7b2c3bdc8f4462d65b5a9c762c5167c29620d4
Stefan Matting <stefan@wire.com>
2024-09-26 15:15:56 +0200

    bump wire-builds link

--- offline/ci.sh
+++ offline/ci.sh
@@ -202,7 +202,7 @@ pull_charts() {
   echo "Pulling charts done."
 }
 
-wire_build="https://raw.githubusercontent.com/wireapp/wire-builds/a09d9ce86def35784fece9ade9fcbc62b87303c5/build.json"
+wire_build="https://raw.githubusercontent.com/wireapp/wire-builds/5c138d5ac8d5aea8e2e6270c7c836ba5bc28c2ae/build.json"
 wire_build_chart_release "$wire_build" | pull_charts
 
 # Uncomment if you want to create non-wire-build release
