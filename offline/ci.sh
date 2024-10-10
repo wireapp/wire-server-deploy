94af95a64680ee4cdeb47ef6ae5a3b9353e5881e
Amit Sagtani <asagtani06@gmail.com>
2024-10-10 15:26:05 +0200

    update docs

--- offline/ci.sh
+++ offline/ci.sh
@@ -239,6 +239,6 @@ tar cf containers-helm.tar containers-helm
 
 echo "docker_ubuntu_repo_repokey: '${fingerprint}'" > ansible/inventory/offline/group_vars/all/key.yml
 
-tar czf assets.tgz debs-jammy.tar binaries.tar containers-adminhost containers-helm.tar containers-other.tar containers-system.tar ansible charts values bin
+tar czf assets.tgz debs-jammy.tar binaries.tar containers-adminhost containers-helm.tar containers-system.tar ansible charts values bin
 
 echo "Done"
