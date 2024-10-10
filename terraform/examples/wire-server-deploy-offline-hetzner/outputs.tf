061aef13227804ef3c8996e681c3a681e9d65e46
Amit Sagtani <asagtani06@gmail.com>
2024-10-10 14:35:43 +0200

    remove restund references and code

--- terraform/examples/wire-server-deploy-offline-hetzner/outputs.tf
+++ terraform/examples/wire-server-deploy-offline-hetzner/outputs.tf
@@ -96,17 +96,5 @@ output "static-inventory" {
         minio_network_interface = "ens10"
       }
     }
-    restund = {
-      hosts = {
-        for index, server in hcloud_server.restund : server.name => {
-          ansible_host = hcloud_server_network.restund[index].ip
-          ansible_user = "root"
-        }
-      }
-      vars = {
-        restund_network_interface = "ens10"
-      }
-    }
-
   }
 }
