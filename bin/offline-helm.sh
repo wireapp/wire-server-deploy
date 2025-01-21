e9a8a7f4c52356d8332bc354a5454c00e2080175
Vedran Ivanković <33936733+Veki301@users.noreply.github.com>
2025-01-21 15:18:15 +0100

    return --wait options, sleep at the end of offline-helm to verify pod status

--- bin/offline-helm.sh
+++ bin/offline-helm.sh
@@ -11,27 +11,27 @@ helm upgrade --install --wait demo-smtp ./charts/demo-smtp --values ./values/dem
 helm upgrade --install --wait rabbitmq ./charts/rabbitmq --values ./values/rabbitmq/prod-values.example.yaml --values ./values/rabbitmq/prod-secrets.example.yaml
 helm upgrade --install --wait databases-ephemeral ./charts/databases-ephemeral --values ./values/databases-ephemeral/prod-values.example.yaml
 helm upgrade --install --wait reaper ./charts/reaper
-helm upgrade --install wire-server ./charts/wire-server --values ./values/wire-server/prod-values.example.yaml --values ./values/wire-server/secrets.yaml
-sleep 15m
+helm upgrade --install --wait --timeout=15m0s wire-server ./charts/wire-server --values ./values/wire-server/prod-values.example.yaml --values ./values/wire-server/secrets.yaml
+
 # if charts/webapp directory exists
 if [ -d "./charts/webapp" ]; then
-    helm upgrade --install  webapp ./charts/webapp --values ./values/webapp/prod-values.example.yaml
+    helm upgrade --install --wait webapp ./charts/webapp --values ./values/webapp/prod-values.example.yaml
 fi
 
 if [ -d "./charts/account-pages" ]; then
-    helm upgrade --install account-pages ./charts/account-pages --values ./values/account-pages/prod-values.example.yaml
+    helm upgrade --install --wait account-pages ./charts/account-pages --values ./values/account-pages/prod-values.example.yaml
 fi
 
 if [ -d "./charts/team-settings" ]; then
-    helm upgrade --install team-settings ./charts/team-settings --values ./values/team-settings/prod-values.example.yaml --values ./values/team-settings/prod-secrets.example.yaml
+    helm upgrade --install --wait team-settings ./charts/team-settings --values ./values/team-settings/prod-values.example.yaml --values ./values/team-settings/prod-secrets.example.yaml
 fi
 
-helm upgrade --install smallstep-accomp ./charts/smallstep-accomp --values ./values/smallstep-accomp/prod-values.example.yaml
-helm upgrade --install ingress-nginx-controller ./charts/ingress-nginx-controller --values ./values/ingress-nginx-controller/hetzner-ci.example.yaml
+helm upgrade --install --wait smallstep-accomp ./charts/smallstep-accomp --values ./values/smallstep-accomp/prod-values.example.yaml
+helm upgrade --install --wait ingress-nginx-controller ./charts/ingress-nginx-controller --values ./values/ingress-nginx-controller/hetzner-ci.example.yaml
 
-sleep 15m
 echo "Printing all pods status: "
 kubectl get pods --all-namespaces -o wide
-./bin/debug_logs.sh
+sleep 1m
+#./bin/debug_logs.sh
 # TODO: Requires certs; which we do not have in CI/CD at this point. future work =) (Would need cert-manager in offline package. That'd be neat)
 # helm upgrade --install --wait nginx-ingress-services ./charts/nginx-ingress-services --values ./values/nginx-ingress-services/values.yaml  --values ./values/nginx-ingress-services/secrets.yaml
