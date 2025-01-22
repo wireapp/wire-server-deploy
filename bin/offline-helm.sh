8e9225211f2d3a9f928b915cee112b9173cb6cb7
Vedran Ivanković <33936733+Veki301@users.noreply.github.com>
2025-01-22 11:15:15 +0100

    more logging and pauses

--- bin/offline-helm.sh
+++ bin/offline-helm.sh
@@ -13,6 +13,11 @@ helm upgrade --install --wait databases-ephemeral ./charts/databases-ephemeral -
 helm upgrade --install --wait reaper ./charts/reaper
 helm upgrade --install --wait --timeout=30m0s wire-server ./charts/wire-server --values ./values/wire-server/prod-values.example.yaml --values ./values/wire-server/secrets.yaml
 
+sleep 15m
+kubectl get pods --all-namespaces -o wide
+sleep 10m
+kubectl get pods --all-namespaces -o wide
+
 # if charts/webapp directory exists
 if [ -d "./charts/webapp" ]; then
     helm upgrade --install --wait webapp ./charts/webapp --values ./values/webapp/prod-values.example.yaml
@@ -32,6 +37,6 @@ helm upgrade --install --wait ingress-nginx-controller ./charts/ingress-nginx-co
 echo "Printing all pods status: "
 kubectl get pods --all-namespaces -o wide
 sleep 1m
-#./bin/debug_logs.sh
+./bin/debug_logs.sh
 # TODO: Requires certs; which we do not have in CI/CD at this point. future work =) (Would need cert-manager in offline package. That'd be neat)
 # helm upgrade --install --wait nginx-ingress-services ./charts/nginx-ingress-services --values ./values/nginx-ingress-services/values.yaml  --values ./values/nginx-ingress-services/secrets.yaml
