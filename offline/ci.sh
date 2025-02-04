2ec6d66c81ac7d6ae9784dc618daedc4892c6f1f
Vedran Ivanković <33936733+Veki301@users.noreply.github.com>
2025-02-04 14:58:27 +0100

    bump nginx

--- offline/ci.sh
+++ offline/ci.sh
@@ -78,7 +78,7 @@ quay.io/jetstack/cert-manager-controller:v1.16.3
 quay.io/jetstack/cert-manager-cainjector:v1.16.3
 quay.io/jetstack/cert-manager-webhook:v1.16.3
 quay.io/jetstack/cert-manager-ctl:v1.14.7
-docker.io/library/nginx:1.25.2-alpine
+docker.io/library/nginx:1.25.4-alpine
 quay.io/wire/ldap-scim-bridge:0.9
 bats/bats:1.11.1
 docker.io/openebs/linux-utils:3.5.0
