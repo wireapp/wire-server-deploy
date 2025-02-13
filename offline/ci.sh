f993657ef0f65939725e1a80b69caa43a242233d
Vedran Ivanković <33936733+Veki301@users.noreply.github.com>
2025-02-13 11:45:29 +0100

    remove openebs from containers-system images, it should come from wire helm charts

--- offline/ci.sh
+++ offline/ci.sh
@@ -73,7 +73,6 @@ quay.io/jetstack/cert-manager-webhook:v1.16.3
 quay.io/jetstack/cert-manager-startupapicheck:v1.16.3
 docker.io/library/nginx:1.25.4-alpine
 bats/bats:1.11.1
-docker.io/openebs/linux-utils:3.5.0
 docker.io/datastax/cass-config-builder:1.0-ubi8
 docker.io/k8ssandra/cass-management-api:3.11.16
 docker.io/k8ssandra/system-logger:v1.19.1
