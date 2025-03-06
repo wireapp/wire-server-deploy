967aa16ec3ca8677230c7c197b81e2fa48801f3f
Vedran Ivanković <33936733+Veki301@users.noreply.github.com>
2025-03-06 14:53:23 +0100

    debug

--- nix/scripts/create-container-dump.sh
+++ nix/scripts/create-container-dump.sh
@@ -13,6 +13,7 @@ mkdir -p $1
 # If this errors out for you, copy default-policy.json from the skopeo repo to
 # /etc/containers/policy.json
 while IFS= read -r image; do
+    echo $image
     # sanitize the image file name, replace slashes with underscores, suffix with .tar
     image_filename=$(sed -r "s/[:\/]/_/g" <<< $image)
     image_path=$(realpath $1)/${image_filename}.tar
