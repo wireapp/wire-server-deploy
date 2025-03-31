76f01f1b3ef6fa8142dc5a0a8b2c3241fb8fc95c
mohit rajain <20745774+mohitrajain@users.noreply.github.com>
2025-03-31 20:28:26 +0200

    fix output_dir var proc_wire_binaries.sh

--- offline/tasks/proc_wire_binaries.sh
+++ offline/tasks/proc_wire_binaries.sh
@@ -2,14 +2,14 @@
 set -euo pipefail
 
 if [[ ! $# -eq 1 ]]; then
-  echo "usage: $0 OUTPUT-DIR" >&2
+  echo "usage: $0 OUTPUT_DIR" >&2
   exit 1
 fi
 
 OUTPUT_DIR="$1"
 
-echo "Processing wire binaries ${OUTPUT-DIR} ..."
+echo "Processing wire binaries ${OUTPUT_DIR} ..."
 
-install -m755 "$(nix-build --no-out-link -A pkgs.wire-binaries)/"* ${OUTPUT-DIR}/binaries/
-tar cf ${OUTPUT-DIR}/binaries.tar ${OUTPUT-DIR}/binaries
-rm -r ${OUTPUT-DIR}/binaries
+install -m755 "$(nix-build --no-out-link -A pkgs.wire-binaries)/"* ${OUTPUT_DIR}/binaries/
+tar cf ${OUTPUT_DIR}/binaries.tar ${OUTPUT_DIR}/binaries
+rm -r ${OUTPUT_DIR}/binaries
