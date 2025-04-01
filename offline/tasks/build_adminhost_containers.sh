3611b68373c9cac1526124b4a60d971f7760aa9e
mohitrajain <20745774+mohitrajain@users.noreply.github.com>
2025-04-01 17:04:58 +0200

    WPB-16722: fix the relative paths while archieving in tasks

--- offline/tasks/build_adminhost_containers.sh
+++ offline/tasks/build_adminhost_containers.sh
@@ -10,10 +10,15 @@ OUTPUT_DIR="$1"
 
 echo "Building admin container images ${OUTPUT_DIR} ..."
 
+ORIGINAL_DIR="$PWD"
+cd "${OUTPUT_DIR}" || { echo "Error: Cannot change to directory ${OUTPUT_DIR}/debs-jammy"; exit 1; }
+
 # Build the container image
 container_image=$(nix-build --no-out-link -A container)
-install -m755 "$container_image" "${OUTPUT_DIR}/containers-adminhost/container-wire-server-deploy.tgz"
+install -m755 "$container_image" containers-adminhost/container-wire-server-deploy.tgz"
 
 # Download zauth; as it's needed to generate certificates
-wire_version=$(helm show chart "${OUTPUT_DIR}"/charts/wire-server | yq -r .version)
-echo "quay.io/wire/zauth:$wire_version" | create-container-dump "${OUTPUT_DIR}"/containers-adminhost
+wire_version=$(helm show chart charts/wire-server | yq -r .version)
+echo "quay.io/wire/zauth:$wire_version" | create-container-dump containers-adminhost
+
+cd "$ORIGINAL_DIR" || { echo "Error: Cannot change back to original directory"; exit 1; }
--- offline/tasks/proc_wire_binaries.sh
+++ offline/tasks/proc_wire_binaries.sh
@@ -10,6 +10,11 @@ OUTPUT_DIR="$1"
 
 echo "Processing wire binaries ${OUTPUT_DIR} ..."
 
-install -m755 "$(nix-build --no-out-link -A pkgs.wire-binaries)/"* "${OUTPUT_DIR}"/binaries/
-tar cf "${OUTPUT_DIR}"/binaries.tar "${OUTPUT_DIR}"/binaries
-rm -r "${OUTPUT_DIR}"/binaries
+ORIGINAL_DIR="$PWD"
+cd "${OUTPUT_DIR}" || { echo "Error: Cannot change to directory ${OUTPUT_DIR}/debs-jammy"; exit 1; }
+
+install -m755 "$(nix-build --no-out-link -A pkgs.wire-binaries)/"* binaries/
+tar cf binaries.tar binaries
+rm -r binaries
+
+cd "$ORIGINAL_DIR" || { echo "Error: Cannot change back to original directory"; exit 1; }
