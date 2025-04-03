7123d493aa051c66f278d08729992f89d648bf10
mohitrajain <20745774+mohitrajain@users.noreply.github.com>
2025-04-03 18:24:39 +0200

    WPB-16722: add debugging statements to identify the issue

--- nix/scripts/create-container-dump.sh
+++ nix/scripts/create-container-dump.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # This consumes a list of containers from stdin and produces a `skopeo sync`
 # dir at $1.
-set -eou pipefail
+set -x -eou pipefail
 
 if [[ ! $# -eq 1 ]]; then
   echo "usage: $0 OUTPUT-DIR" >&2
