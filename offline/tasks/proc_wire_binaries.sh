0ba9b0b09b708616cc5ed53c98b8814ace97cf27
mohitrajain <20745774+mohitrajain@users.noreply.github.com>
2025-04-07 15:10:28 +0200

    WPB-16100: fix wire-binaries.json logic in proc_wire_binaries.sh

--- offline/tasks/proc_wire_binaries.sh
+++ offline/tasks/proc_wire_binaries.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -x -euo pipefail
+set -euo pipefail
 
 if [[ ! $# -eq 2 ]]; then
   echo "usage: $0 OUTPUT_DIR ROOT_DIR" >&2
@@ -11,20 +11,21 @@ ROOT_DIR="$2"
 
 echo "Processing wire binaries ${OUTPUT_DIR} ..."
 
-install -m755 "$(nix-build --no-out-link -A pkgs.wire-binaries)/"* "${OUTPUT_DIR}"/binaries/
+#install -m755 "$(nix-build --no-out-link -A pkgs.wire-binaries)/"* "${OUTPUT_DIR}"/binaries/
 
-tar cf "${OUTPUT_DIR}"/binaries.tar -C "${OUTPUT_DIR}" binaries
-rm -r "${OUTPUT_DIR}"/binaries
+#tar cf "${OUTPUT_DIR}"/binaries.tar -C "${OUTPUT_DIR}" binaries
+#rm -r "${OUTPUT_DIR}"/binaries
 
 function write_wire_binaries_json() {
   temp_dir=$(mktemp -d -p "${OUTPUT_DIR}")
 
   # "Get" all the binaries from the .nix file
+  ls "${ROOT_DIR}/nix/pkgs/wire-binaries.nix"
   sed -n '/_version/p' "${ROOT_DIR}/nix/pkgs/wire-binaries.nix" | grep -v '\.version' | grep -v 'url' > "${temp_dir}/wire-binaries.json.tmp"
 
-  echo "[" > "${temp_dir}/wire-binaries.json.tmp"
+  echo "[" > "${temp_dir}/wire-binaries.json.formatted"
   # Format it into JSON
-  sed -E '/\.url|\.version/!s/([a-z_]+)_version = "(.*)";/{\n  "\1": { "version": "\2" }\n},/' "${temp_dir}/wire-binaries.json.tmp" > "${temp_dir}/wire-binaries.json.formatted"
+  sed -E '/\.url|\.version/!s/([a-z_]+)_version = "(.*)";/{\n  "\1": { "version": "\2" }\n},/' "${temp_dir}/wire-binaries.json.tmp" >> "${temp_dir}/wire-binaries.json.formatted"
   # remove trailing comma -.-
   sed -i '$ s/,$//' "${temp_dir}/wire-binaries.json.formatted"
   
