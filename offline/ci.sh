6689c8be15142e6e80a9d4f942e8592124a335bf
Zebot <zebot@users.noreply.github.com>
2025-04-08 14:21:17 +0000

    Update wire-build to version https://raw.githubusercontent.com/wireapp/wire-builds/ddb4109cf573f883d022a2281cabd3f965382d3f/build.json

--- offline/ci.sh
+++ offline/ci.sh
@@ -190,7 +190,7 @@ pull_charts() {
   echo "Pulling charts done."
 }
 
-wire_build="https://raw.githubusercontent.com/wireapp/wire-builds/5443da20184574e097ec71a048cfc32d9f85a628/build.json"
+wire_build="https://raw.githubusercontent.com/wireapp/wire-builds/ddb4109cf573f883d022a2281cabd3f965382d3f/build.json"
 wire_build_chart_release "$wire_build" | pull_charts
 
 # Uncomment if you want to create non-wire-build release
