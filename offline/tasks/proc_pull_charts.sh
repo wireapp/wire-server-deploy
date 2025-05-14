6ae6437e994fdbcbfe6dc60cdfb8c43a6fecfbc8
Zebot <zebot@users.noreply.github.com>
2025-05-14 07:53:11 +0000

    Update wire-build to version https://raw.githubusercontent.com/wireapp/wire-builds/4aeae76afd751231838fbf29e9b39bac1526e8f3/build.json

--- offline/tasks/proc_pull_charts.sh
+++ offline/tasks/proc_pull_charts.sh
@@ -83,5 +83,5 @@ pull_charts() {
   echo "Pulling charts done."
 }
 
-wire_build="https://raw.githubusercontent.com/wireapp/wire-builds/fe313e3f05d8fba240efad43c2985ebbc0bead27/build.json"
+wire_build="https://raw.githubusercontent.com/wireapp/wire-builds/4aeae76afd751231838fbf29e9b39bac1526e8f3/build.json"
 wire_build_chart_release "$wire_build" | pull_charts
