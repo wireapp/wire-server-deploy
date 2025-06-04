5fc74aeb140eb5ad38ab2168f3155c57ff464ba0
Vedran Ivanković <33936733+Veki301@users.noreply.github.com>
2025-06-04 15:30:30 +0200

    revert everything, try overriding python with a custom made package

--- default.nix
+++ default.nix
@@ -2,7 +2,6 @@
 
 let
   sources = import ./nix/sources.nix;
-
   # for injecting old gnupg dependency
   oldpkgs = import sources.oldpkgs {
     inherit system;
@@ -25,6 +24,16 @@ let
     ];
   };
 
+  # inject jmespath into a python package
+  pythonWithJmespath = pkgs.python3.withPackages (ps: with ps; [
+    jmespath
+  ]);
+
+  # override python used in pythonForAnsible with our custom jmespath injected package
+  pythonForAnsibleWithJmespath = pkgs.pythonForAnsible.override {
+    python = pythonWithJmespath;
+  };
+
   profileEnv = pkgs.writeTextFile {
     name = "profile-env";
     destination = "/.profile";
@@ -34,23 +43,24 @@ let
     '';
   };
 
-  # Add jmespath to the Ansible Python environment
-  pythonWithJmespath = pkgs.python3.withPackages (ps: with ps; [ jmespath ]);
 
-in rec {
+in
+rec {
   inherit pkgs profileEnv;
 
   env = pkgs.buildEnv {
     name = "wire-server-deploy";
     paths = with pkgs; [
       ansible_2_16
-      pythonWithJmespath
+      pythonForAnsible
+      jmespath
       apacheHttpd
       awscli2
       gnumake
       gnupg1
       # injected dependacy gnupg1orig
       gnupg1orig
+
       kubernetes-tools
 
       # Note: This is overriden in nix/overlay.nix to have plugins. This is
@@ -104,7 +114,7 @@ in rec {
       pkgs.bashInteractive
       pkgs.openssh # ansible needs this too, even with paramiko
       pkgs.sshpass # needed for password login
-      pythonWithJmespath
+
       # The enivronment
       env
       # provide /usr/bin/env and /tmp in the container too :-)
@@ -121,8 +131,7 @@ in rec {
       Env = [
         "KUBECONFIG=/wire-server-deploy/ansible/inventory/offline/artifacts/admin.conf"
         "ANSIBLE_CONFIG=/wire-server-deploy/ansible/ansible.cfg"
-        "LOCALHOST_PYTHON=${env}/bin/python"
-        "ANSIBLE_PYTHON_INTERPRETER=/bin/python" # TEST:
+        "LOCALHOST_PYTHON=${env}/bin/python" # is this even referencing the correct python used by ansible and is this even relevant for setting what is being used?
       ];
     };
   };
