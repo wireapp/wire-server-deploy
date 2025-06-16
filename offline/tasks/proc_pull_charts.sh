dac062ca21ea03eec57754005bacf141a0a66615
sghosh23 <amisukanta02@gmail.com>
2025-06-16 11:41:04 +0200

    Update Instrument_monitoring.md with additional instructions for values.yaml and issuer verification; modify proc_pull_charts.sh to use the latest wire build URL

--- offline/tasks/proc_pull_charts.sh
+++ offline/tasks/proc_pull_charts.sh
@@ -83,5 +83,5 @@ pull_charts() {
   echo "Pulling charts done."
 }
 
-wire_build="https://raw.githubusercontent.com/wireapp/wire-builds/030b549c6a2a343aaef5d542a9af17b09cd11713/build.json"
+wire_build="https://raw.githubusercontent.com/wireapp/wire-builds/07012185d4d18a5441995a815f94e2c5acf2a857/build.json"
 wire_build_chart_release "$wire_build" | pull_charts
