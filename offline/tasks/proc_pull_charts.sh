5e795b006ec761c8db74b813c83882f75f61bc76
sghosh23 <amisukanta02@gmail.com>
2025-06-17 12:06:40 +0200

    Update wire build URL in proc_pull_charts.sh to the latest version

--- offline/tasks/proc_pull_charts.sh
+++ offline/tasks/proc_pull_charts.sh
@@ -83,5 +83,5 @@ pull_charts() {
   echo "Pulling charts done."
 }
 
-wire_build="https://raw.githubusercontent.com/wireapp/wire-builds/07012185d4d18a5441995a815f94e2c5acf2a857/build.json"
+wire_build="https://raw.githubusercontent.com/wireapp/wire-builds/47630a95de3f0442050c1dd9f42a8b65e02be50e/build.json"
 wire_build_chart_release "$wire_build" | pull_charts
