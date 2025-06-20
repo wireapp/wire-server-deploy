4264a0052cb87dfae6e5a9ae2459a2255d2494ca
Mohit Rajain <20745774+mohitrajain@users.noreply.github.com>
2025-06-20 11:52:31 +0200

    overwrite the image for postgress dependency for volume versions

--- nix/scripts/list-helm-containers.sh
+++ nix/scripts/list-helm-containers.sh
@@ -5,7 +5,7 @@
 # those.
 # In cases where no container image tag has been specified, it'll use `latest`.
 # The list is sorted and deduplicated, then printed to stdout.
-set -eou pipefail
+set -eoux pipefail
 
 VALUES_DIR=""
 HELM_IMAGE_TREE_FILE=""
