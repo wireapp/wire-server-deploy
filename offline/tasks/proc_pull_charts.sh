a4e3cc3862bebdfa86b28462de7f2cf56e38bbe6
sghosh23 <amisukanta02@gmail.com>
2025-06-30 15:00:00 +0200

    test-coturn-metrics: update wire build url

--- offline/tasks/proc_pull_charts.sh
+++ offline/tasks/proc_pull_charts.sh
@@ -83,5 +83,5 @@ pull_charts() {
   echo "Pulling charts done."
 }
 
-wire_build="https://raw.githubusercontent.com/wireapp/wire-builds/6ba5988ca1ce1731e1ea22e5a63e7d8f688240aa/build.json"
+wire_build="https://raw.githubusercontent.com/wireapp/wire-builds/99e1763f3b15779530de30873b72f2da2fe6f721/build.json"
 wire_build_chart_release "$wire_build" | pull_charts
