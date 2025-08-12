7f6ed881d6682d7eb4f86cf6e3da4c7e114456b2
mohitrajain <20745774+mohitrajain@users.noreply.github.com>
2025-08-12 18:00:32 +0200

    feat(postgres): WPB-17218 disable terraform destroy on failure and add more debugging

--- offline/cd.sh
+++ offline/cd.sh
@@ -2,11 +2,11 @@
 
 set -euo pipefail
 
-function cleanup {
-  (cd terraform/examples/wire-server-deploy-offline-hetzner ; terraform destroy -auto-approve)
-  echo done
-}
-trap cleanup EXIT
+#function cleanup {
+#  (cd terraform/examples/wire-server-deploy-offline-hetzner ; terraform destroy -auto-approve)
+#  echo done
+#}
+#trap cleanup EXIT
 (cd terraform/examples/wire-server-deploy-offline-hetzner ; terraform init ; terraform apply -auto-approve )
 adminhost=$(cd terraform/examples/wire-server-deploy-offline-hetzner ; terraform output adminhost)
 adminhost="${adminhost//\"/}" # remove extra quotes around the returned string
@@ -17,9 +17,10 @@ ssh-add - <<< "$ssh_private_key"
 
 ssh -oStrictHostKeyChecking=accept-new -oConnectionAttempts=10 "root@$adminhost" tar xzv < offline/default-build/output/assets.tgz
 
+ssh "root@$adminhost" cat ./ansible/inventory/offline/inventory.yml || true
+terraform output ssh_private_key || true
+
 (cd terraform/examples/wire-server-deploy-offline-hetzner; terraform output -json static-inventory)| ssh "root@$adminhost" tee ./ansible/inventory/offline/inventory.yml
 
-ssh "root@$adminhost" cat ./ansible/inventory/offline/inventory.yml || true
-
 # NOTE: Agent is forwarded; so that the adminhost can provision the other boxes
-ssh -A "root@$adminhost" ./bin/offline-deploy.sh || true
+ssh -A "root@$adminhost" ./bin/offline-deploy.sh
