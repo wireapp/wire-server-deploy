b463f5020869a74250aa9646467b175694f53215
mohitrajain <mohit.rajain@wire.com>
2026-01-05 12:14:58 +0100

    fix: wpb-22439 fix handling of bitnami images for old charts

--- offline/tasks/proc_pull_charts.sh
+++ offline/tasks/proc_pull_charts.sh
@@ -81,17 +81,6 @@ pull_charts() {
     (cd "${OUTPUT_DIR}"/charts; helm pull --version "$version" --untar "$repo_short_name/$name")
   done
   echo "Pulling charts done."
-
-  # Patch bitnami repository references in pulled charts
-  # Remove the extraction and replacement when there will be no more bitnami charts
-  #echo "Patching bitnami repository references..."
-  #SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-  #PATCH_SCRIPT="${SCRIPT_DIR}/patch-chart-images.sh"
-  #if [[ -f "$PATCH_SCRIPT" ]]; then
-  #  "$PATCH_SCRIPT" "${OUTPUT_DIR}/charts"
-  #else
-  #  echo "Warning: patch-chart-images.sh not found at $PATCH_SCRIPT, skipping chart patching"
-  #fi
 }
 
 wire_build="https://raw.githubusercontent.com/wireapp/wire-builds/refs/heads/wiab-dev-514/build.json"
