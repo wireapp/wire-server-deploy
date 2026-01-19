4629201cc83e7fa69fd7c6c32957c07c91257507
mohitrajain <mohit.rajain@wire.com>
2026-01-19 19:04:36 +0100

    fix: wpb-22439 bring changes from PR848

--- bin/offline-deploy.sh
+++ bin/offline-deploy.sh
@@ -41,16 +41,4 @@ fi
 
 $DOCKER_RUN_BASE $SSH_MOUNT $WSD_CONTAINER ./bin/offline-cluster.sh
 
-# Sync PostgreSQL password from K8s secret to secrets.yaml
-#echo "Syncing PostgreSQL password from Kubernetes secret..."
-#sudo docker run --network=host -v $PWD:/wire-server-deploy $WSD_CONTAINER ./bin/sync-k8s-secret-to-wire-secrets.sh \
-#  wire-postgresql-external-secret \
-#  password \
-#  values/wire-server/secrets.yaml \
-#  .brig.secrets.pgPassword \
-#  .galley.secrets.pgPassword \
-#  .spar.secrets.pgPassword \
-#  .gundeck.secrets.pgPassword
-
-
-sudo docker run --network=host -v $PWD:/wire-server-deploy $WSD_CONTAINER ./bin/offline-helm.sh
+sudo docker run --network=host -v $PWD:/wire-server-deploy $WSD_CONTAINER ./bin/helm-operations.sh
