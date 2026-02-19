3f44be28ce63086648f4d12321fae2867cefd3bb
mohitrajain <mohit.rajain@wire.com>
2026-02-19 18:21:00 +0100

    fix: wpb-23462: fix pre_chart_process

--- offline/tasks/pre_chart_process_0.sh
+++ offline/tasks/pre_chart_process_0.sh
@@ -39,4 +39,4 @@ sed -i -Ee 's/useSharedFederatorSecret: false/useSharedFederatorSecret: true/' "
 
 # drop step-certificates/.../test-connection.yaml because it lacks an image tag
 # cf. https://github.com/smallstep/helm-charts/pull/196/files
-rm -v "${OUTPUT_DIR}"/charts/step-certificates/charts/step-certificates/templates/tests/*
+# rm -v "${OUTPUT_DIR}"/charts/step-certificates/charts/step-certificates/templates/tests/*
