3bbf9a89e50d51a9d00ae3a9020b182feebba828
mohitrajain <mohit.rajain@wire.com>
2026-06-25 15:26:41 +0200

    fix: WPB-26513 Update the wire-builds reference for 2026-q2

--- offline/tasks/proc_pull_charts.sh
+++ offline/tasks/proc_pull_charts.sh
@@ -85,5 +85,5 @@ pull_charts() {
 
 }
 
-wire_build="https://raw.githubusercontent.com/wireapp/wire-builds/818524e35d2894f5486c50b9ed9ed967ac099561/build.json"
+wire_build="https://raw.githubusercontent.com/wireapp/wire-builds/refs/heads/2026-q2/build.json"
 wire_build_chart_release "$wire_build" | pull_charts
