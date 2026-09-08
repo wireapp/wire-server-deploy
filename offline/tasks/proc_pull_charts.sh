512c70bda6798503f8b7745b8300fd63587a68d5
Vedran Ivanković <33936733+Veki301@users.noreply.github.com>
2026-09-08 12:14:23 +0200

    build 2025-q2-hotfix bundle

--- offline/tasks/proc_pull_charts.sh
+++ offline/tasks/proc_pull_charts.sh
@@ -84,5 +84,5 @@ pull_charts() {
 
 }
 
-wire_build="https://raw.githubusercontent.com/wireapp/wire-builds/818524e35d2894f5486c50b9ed9ed967ac099561/build.json"
+wire_build="https://raw.githubusercontent.com/wireapp/wire-builds/refs/heads/q2-2025/build.json"
 wire_build_chart_release "$wire_build" | pull_charts
