7188303e0f34ad1f223312d1952796e9901d7ec6
mohit rajain <mohit.rajain@wire.com>
2026-09-24 09:44:46 +0200

    fix: WPB-25755 fix for workflows when pulling images and connecting to new VMs (#919)
    * fix: WPB-25755 update the docker pull logic to retry if initial pull fails and wait when connecting to fresh VMs in hetzner for cd
    
    * fix: WPB-25755 handle the skopeo copy failure better
    
    * Wpb 26513 dump build version info (#920)
    
    * fix: WPB-26513 Dump version information post creating the artifacts on the stdout
    
    * fix: WPB-26513 remove the test exit debug command
    
    * add: WPB-26513 fix minor file-name issue

--- offline/tasks/proc_pull_charts.sh
+++ offline/tasks/proc_pull_charts.sh
@@ -36,13 +36,14 @@ echo "Excluding following charts from the release: $HELM_CHART_EXCLUDE_LIST"
 wire_build_chart_release () {
 
   wire_build="$1"
-  curl "$wire_build" | jq -r --argjson HELM_CHART_EXCLUDE_LIST "$HELM_CHART_EXCLUDE_LIST" '
+  curl "$wire_build" -o "${OUTPUT_DIR}/build.json"
+  jq -r --argjson HELM_CHART_EXCLUDE_LIST "$HELM_CHART_EXCLUDE_LIST" '
   .helmCharts
   | with_entries(select(.key as $k | $HELM_CHART_EXCLUDE_LIST | index($k) | not))
   | to_entries
   | map("\(.key) \(.value.repo) \(.value.version)")
   | join("\n")
-  '
+  ' "${OUTPUT_DIR}/build.json"
 }
 
 # pull_charts() accepts charts in format
